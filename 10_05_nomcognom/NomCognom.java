147743bba494ab867e8271ae5c43e22fac8b1217
Iker <xshine555@gmail.com>
2024-09-23 13:09:07 +0200

    Zzz

---
+++ 10_05_nomcognom/NomCognom.java
@@ -0,0 +1,25 @@
+/*
+ * Aquest programa demana el nom i el primer cognom als usuaris
+ * i els mostra un missatge amistós.
+ *
+ * El programa no controla valors no adequats d'entrada
+ */
+import java.util.*;
+
+public class NomCognom {
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+
+        // obté el nom
+        System.out.println("Nom?");
+        String nom = scanner.nextLine();
+
+
+        // obté el cognom
+        System.out.println("Cognoms?");
+        String cognom = scanner.nextLine();
+
+        // Mostra resultat
+        System.out.println("Ei " + nom + ", tinc una amiga que també és " +  cognom);
+    }
+}
