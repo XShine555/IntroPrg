5049112ce7743b61b583348c3fef8964e64b6f6c
prgtest <xshine555@gmail.com>
2024-10-04 12:51:00 +0200

    Sem  ∾ prgtest autocommit 11_14 #1

--- 11_14_semafor/Semafor.java
+++ 11_14_semafor/Semafor.java
@@ -1,5 +1,6 @@
 public class Semafor {
 	public static void main(String[] args) {
+		System.out.println("Color?");
 		String color = Entrada.readLine();
 		if (color.equals("vermell")) {
 			System.out.println("espera");
