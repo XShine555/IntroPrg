5896944ed43b48966d8ee9af1686ebd5b2072c70
Iker <xshine555@gmail.com>
2024-10-04 13:08:29 +0200

    Arriba

---
+++ 11_16_ascensor/Ascensor.java
@@ -0,0 +1,37 @@
+public class Ascensor {
+	public static void main(String[] args) {
+		System.out.println("pis?");
+		String pis = Entrada.readLine();
+		System.out.println("botó?");
+		String boto = Entrada.readLine();
+
+		String nuevoPis = "";
+
+		switch (pis) {
+			case "planta baixa":
+				nuevoPis = switch (boto) {
+					case "pujar dos" -> "segon pis";
+					case "pujar un" ->  "primer pis";
+					default -> "error";
+				};
+				break;
+			case "primer pis":
+				nuevoPis = switch (boto) {
+					case "pujar un" -> "segon pis";
+					case "baixar un" -> "planta baixa";
+					default -> "error";
+				};
+				break;
+			case "segon pis":
+				nuevoPis = switch (boto) {
+					case "baixar un" -> "primer pis";
+					case "baixar dos" -> "planta baixa";
+					default -> "error";
+				};
+			default:
+				nuevoPis = "error";
+				break;
+		}
+		System.out.println(nuevoPis);
+	}
+}
