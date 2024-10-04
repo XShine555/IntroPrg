e8ba06fb7b068d6efb971b90c4dd63d1076db43e
Iker <xshine555@gmail.com>
2024-10-04 10:22:30 +0200

    11_04

---
+++ 11_04_ternari/ParellSenar.java
@@ -0,0 +1,11 @@
+/*
+ * Programa que indica si el número proporcionat com a primer argument
+ * és parell o senar.
+ * Si no s'especifica cap número, el resultat és indeterminat.
+ */
+public class ParellSenar {
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(args[0]);
+        System.out.println(numero % 2 == 0 ? String.format("Él número %s és parell", numero) : String.format("El número %s és senar", numero));
+    }
+}
