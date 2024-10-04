fc55992b03793620bd58292110c8f0f5be345b89
Iker <xshine555@gmail.com>
2024-10-04 11:46:20 +0200

    11_10

---
+++ 11_10_mesosanys/MesosAnys.java
@@ -0,0 +1,24 @@
+public class MesosAnys {
+	public static void main(String[] args) {
+		System.out.println("Mes?");
+		int month = Integer.parseInt(Entrada.readLine());
+		System.out.println("Any?");
+		int year = Integer.parseInt(Entrada.readLine());
+
+		int nextMonth = month + 1;
+		int nextYear = year + 1;
+		if (nextMonth > 12) {
+			nextMonth = 1;
+			nextYear++;
+		}
+
+		int lastMonth = month - 1;
+		int lastYear = year - 1;
+		if (lastMonth < 1) {
+			lastMonth = 12;
+			lastYear--;
+		}
+
+		System.out.println(String.format("Mes anterior %s/%s i mes seguent %s/%s", lastMonth, lastYear, nextMonth, nextYear) );
+	}
+}
