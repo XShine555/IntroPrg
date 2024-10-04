0c55b9625da49419282dcfc83b12dd5d4ce58706
Iker <xshine555@gmail.com>
2024-10-04 12:45:37 +0200

    Abs

---
+++ 11_13_pagaments/Pagament.java
@@ -0,0 +1,20 @@
+public class Pagament {
+	public static void main(String[] args) {
+		System.out.println("Preu?");
+		int preu = Integer.parseInt(Entrada.readLine());
+		System.out.println("Paga?");
+		int paga = Integer.parseInt(Entrada.readLine());
+
+		int total = preu - paga;
+
+		if (total == 0) {
+			System.out.println("No sobra in falta res");
+		}
+		else if (total > 0) {
+			System.out.format("Falten %s€", total);
+		}
+		else {
+			System.out.format("Sobren %s€", Math.abs(total));
+		}
+	}
+}
