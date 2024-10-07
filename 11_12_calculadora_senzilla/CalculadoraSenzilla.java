0152b79e41d7518c2b5f68ba267aa1d04620bf56
prgtest <xshine555@gmail.com>
2024-10-07 08:36:31 +0200

    Cosas  ∾ prgtest autocommit 11_12 #2

--- 11_12_calculadora_senzilla/CalculadoraSenzilla.java
+++ 11_12_calculadora_senzilla/CalculadoraSenzilla.java
@@ -22,7 +22,7 @@ public class CalculadoraSenzilla {
 				System.out.format("%s %s %s = %s", num1, op, num2, num1-num2);
 				break;
 			case '*':
-				System.out.format("%s %s %s = %s", num1, op, num2, num1-num2);
+				System.out.format("%s %s %s = %s", num1, op, num2, num1*num2);
 			case '/':
 				if (num2 == 0) {
 					System.out.println("No es pot dividir entre 0");
