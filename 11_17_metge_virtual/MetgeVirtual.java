c4d21d5c43a0c8d64b26f65266b0cae0cb4f2187
prgtest <xshine555@gmail.com>
2024-10-07 08:43:16 +0200

    A  ∾ prgtest autocommit 11_17 #4

--- 11_17_metge_virtual/MetgeVirtual.java
+++ 11_17_metge_virtual/MetgeVirtual.java
@@ -12,7 +12,7 @@ public class MetgeVirtual {
 					System.out.println("prem paracetamol");
 				}
 				else {
-					System.out.println("pren una aspirina.");
+					System.out.println("prem una aspirina.");
 				}
 				return;
 			}
