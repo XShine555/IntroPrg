a09cee2d33dd3cec9a48d7d81e3fa167d93fb31a
prgtest <xshine555@gmail.com>
2024-10-07 08:50:09 +0200

    A  ∾ prgtest autocommit 11_17 #12

--- 11_17_metge_virtual/MetgeVirtual.java
+++ 11_17_metge_virtual/MetgeVirtual.java
@@ -22,10 +22,10 @@ public class MetgeVirtual {
 				System.out.println("edat?");
 				int edad = Integer.parseInt(Entrada.readLine());
 				if (edad < 12) {
-					System.out.println("pren carmel de mel");
+					System.out.println("prem carmel de mel");
 				}
 				else {
-					System.out.println("carmel d’eucaliptus");
+					System.out.println("prem carmel d’eucaliptus");
 				}
 			}
 		}
@@ -37,10 +37,10 @@ public class MetgeVirtual {
 				System.out.println("edat?");
 				int edad = Integer.parseInt(Entrada.readLine());
 				if (edad < 12) {
-					System.out.println("pren carmel de mel");
+					System.out.println("prem carmel de mel");
 				}
 				else {
-					System.out.println("carmel d’eucaliptus");
+					System.out.println("prem carmel d’eucaliptus");
 				}
 			}
 		}
