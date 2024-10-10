8f2806115c958e19000dbeac4859d35a85124172
Iker <xshine555@gmail.com>
2024-10-10 09:46:49 +0200

    A

--- 11_11_tresenordre/TresEnOrdre.java
+++ 11_11_tresenordre/TresEnOrdre.java
@@ -2,10 +2,8 @@
  * Exercici 11_11. Tres en ordre
  * 04/10/2024
  * Iker Rivera Garcia
- * Programa que ordena tres números de menor a mayor, usando Arrays.Sort();
+ * Programa que ordena tres números de menor a mayor.
  */
-
-import java.util.Arrays;
 
 public class TresEnOrdre {
 	public static void main(String[] args) {
@@ -16,9 +14,42 @@ public class TresEnOrdre {
 		System.out.println("Tercer?");
 		int arg3 = Integer.parseInt(Entrada.readLine());
 
-		int[] intArray = {arg1, arg2, arg3};
-		Arrays.sort(intArray);
+		int num1, num2, num3;
 
-		System.out.format("%d, %d i %d", intArray[0], intArray[1], intArray[2]);
+		if (arg1 > arg2 && arg1 > arg3) {
+			num1 = arg1;
+			if (arg2 > arg3) {
+				num2 = arg2;
+				num3 = arg3;
+			}
+			else {
+				num2 = arg3;
+				num3 = arg2;
+			}
+		}
+		else if (arg2 > arg1 && arg2 > arg3) {
+			num1 = arg2;
+			if (arg3 > arg1) {
+				num2 = arg3;
+				num3 = arg1;
+			}
+			else {
+				num2 = arg1;
+				num3 = arg3;
+			}
+		}
+		else {
+			num1 = arg3;
+			if (arg1 > arg2) {
+				num2 = arg1;
+				num3 = arg2;
+			}
+			else {
+				num2 = arg2;
+				num3 = arg1;
+			}
+		}
+
+		System.out.format("%d, %d i %d", num3, num2, num1);
 	}
 }
