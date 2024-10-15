797d6c09fa13b45157c3d13d16a2872cccb42d7b
Iker <xshine555@gmail.com>
2024-10-15 12:55:10 +0200

    For_2

---
+++ 12_04_quantsparells/QuantsParells.java
@@ -0,0 +1,16 @@
+public class QuantsParells {
+	public static void main(String[] args) {
+		int par = 0;
+		for (;;) {
+			System.out.println("Introdueix un valor");
+			int valor = Integer.parseInt(Entrada.readLine());
+			if (valor < 0) {
+				break;
+			}
+			if (valor % 2 == 0) {
+				par++;
+			}
+		}
+		System.out.format("Nombre de parells introduïts: %", par);
+	}
+}
