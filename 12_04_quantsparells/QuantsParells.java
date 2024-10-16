79ba32ab1f565a4c07fe7e2f6c6b6104d055545a
Iker <xshine555@gmail.com>
2024-10-16 08:48:22 +0200

    Comentarios

--- 12_04_quantsparells/QuantsParells.java
+++ 12_04_quantsparells/QuantsParells.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 12_04. Quants parell
+ * Iker Rivera Garcia
+ * 16/09/2024
+ * Programa que le pedirá al usuario números, cuando introduzca uno negativo dejará de pedir números y pondrá la cantidad de veces que ha introducido un número par.
+*/
+
 public class QuantsParells {
 	public static void main(String[] args) {
 		int par = 0;
