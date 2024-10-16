5442a07dc9677feb3314859fb43bbac39eb86e95
Iker <xshine555@gmail.com>
2024-10-16 08:45:34 +0200

    Coentarios

--- 12_03_sumanotes/SumaNotes.java
+++ 12_03_sumanotes/SumaNotes.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 12_03. Suma notes
+ * Iker Rivera Garcia
+ * 16/09/2024
+ * Programa que te pedirá números infinitos hasta que pongas uno menor a 0 o superior a 100 y te mostrará la suma de los números que has puesto.
+*/
+
 public class SumaNotes {
 	public static void main(String[] args) {
 		int valor;
