25cdb0f6d7233430ad95e04d70182ab98a347d9e
Iker <xshine555@gmail.com>
2024-10-18 12:41:59 +0200

    Fix

--- 12_08_estadistiques/Estadistiques.java
+++ 12_08_estadistiques/Estadistiques.java
@@ -1,7 +1,32 @@
 public class Estadistiques {
 	public static void main(String[] args) {
 		float contador = 0;
+
+		System.out.println("Introdueix un valor");
+		int valor = Integer.parseInt(Entrada.readLine());
 		int min = 0, max = 0, total = 0;
+
+		if (valor >= 0) {
+			min = valor;
+			max = valor;
+			total = valor;
+			contador++;
+			while (valor >= 0) {
+				System.out.println("Introdueix un valor");
+				valor = Integer.parseInt(Entrada.readLine());
+
+				if (valor >= 0) {
+					if (min < valor) {
+						min = valor;
+					}
+					if (valor > max) {
+						max = valor;
+					}
+					total += valor;
+					contador++;
+				}
+			}
+		}
 
 		if (contador == 0) {
 			System.out.println("Cap valor vàlid introduït");
