91f44bd86594f35678b59a947149982eca5d50c0
Iker <xshine555@gmail.com>
2024-10-18 12:55:05 +0200

    A

---
+++ 12_10_endevina/EndevinaNombre.java
@@ -0,0 +1,29 @@
+public class EndevinaNombre {
+	public static void main(String[] args) {
+		int valorInput = 0;
+		int valorPensado = Integer.parseInt(args[0]);
+		do {
+			System.out.println("Ves introduint enters entre 1 i 100 fins que encertis el que jo he pensat");
+			valorInput = Integer.parseInt(Entrada.readLine());
+
+			String message;
+			if (valorInput > valorPensado) {
+				message = "És més gran que " + valorInput;
+			}
+			else if (valorInput < valorPensado) {
+				message = "És més petit que " + valorInput;
+			}
+			else if (valorInput > 100) {
+				message = "Com a màxim 100";
+			}
+			else if (valorInput < 1) {
+				message = "Com a mínim 1";
+			}
+			else {
+				message = "Has encertat!";
+			}
+			System.out.println(message);
+		}
+		while (valorPensado != valorInput);
+	}
+}
