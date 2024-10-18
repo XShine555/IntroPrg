e1fa01096fea85b9544275f35b62d1b768629b22
Iker <xshine555@gmail.com>
2024-10-18 12:04:19 +0200

    Estadistiques

---
+++ 12_08_estadistiques/Estadistiques.java
@@ -0,0 +1,24 @@
+public class Estadistiques {
+	public static void main(String[] args) {
+		float contador = 0;
+		int min = 0, max = 0, total = 0;
+		for (; ;) {
+			System.out.println("Introdueix un valor");
+			int value = Integer.parseInt(Entrada.readLine());
+			if (value < 0 || value > 100) {
+				break;
+			}
+			if (value < min) {
+				min = value;
+			}
+			if (value > max) {
+				max = value;
+			}
+			contador++;
+			total += value;
+		}
+		System.out.println("El mínim és: " + min);
+		System.out.println("La mitjana és: " + total / contador);
+		System.out.println("El màxim és: " + max);
+	}
+}
