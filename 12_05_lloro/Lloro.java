7cef602c0a6293a3822b9924a7074b63e477ee50
Iker <xshine555@gmail.com>
2024-10-18 11:46:02 +0200

    Lloro

---
+++ 12_05_lloro/Lloro.java
@@ -0,0 +1,18 @@
+/*
+ * Exercici 12_05. El lloro
+ * Iker Rivera Garcia
+ * 18/10/2024
+ * Programa que repetirá todo lo que introduzcas hasta que introduzcas un texto en blanco o vacía.
+*/
+public class Lloro {
+	public static void main(String[] args) {
+		for (; ;) {
+			System.out.println("El lloro espera paraula:");
+			String input = Entrada.readLine();
+			if (input.isBlank()) {
+				break;
+			}
+			System.out.println("El lloro repeteix: " + input);
+		}
+	}
+}
