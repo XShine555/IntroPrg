7263b94cb67d5e5d4d2e3b02ca649de05313982d
Iker <xshine555@gmail.com>
2024-10-18 11:53:21 +0200

    Mitjana

---
+++ 12_07_mitjana/Mitjana.java
@@ -0,0 +1,21 @@
+public class Mitjana {
+	public static void main(String[] args) {
+		int contador = 0;
+		int total = 0;
+		for (; ;) {
+			System.out.println("Introdueix un valor");
+			int value = Integer.parseInt(Entrada.readLine());
+			if (value < 0 || value > 100) {
+				break;
+			}
+			contador++;
+			total += value;
+		}
+		if (contador == 0) {
+			System.out.println("Cap nota vàlida introduïda");
+		}
+		else {
+			System.out.println("La mitjana de les notes vàlides és " + total / contador);
+		}
+	}
+}
