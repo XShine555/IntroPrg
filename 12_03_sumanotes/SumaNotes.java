b7381bfa9afbd2a7ca972216d0fc6349787c2298
Iker <xshine555@gmail.com>
2024-10-18 12:35:58 +0200

    C4

--- 12_03_sumanotes/SumaNotes.java
+++ 12_03_sumanotes/SumaNotes.java
@@ -7,15 +7,13 @@
 
 public class SumaNotes {
 	public static void main(String[] args) {
-		int valor;
-		int suma = 0;
-		for (;;) {
+		int suma = 0, valor;
+		System.out.println("Introdueix una nota");
+		valor = Integer.parseInt(Entrada.readLine());
+		while (valor >= 0 && valor <= 100) {
+			suma += valor;
 			System.out.println("Introdueix una nota");
 			valor = Integer.parseInt(Entrada.readLine());
-			if (valor < 0 || valor > 100) {
-				break;
-			}
-			suma += valor;
 		}
 		System.out.format("La suma de les notes vàlides és %s", suma);
 	}
