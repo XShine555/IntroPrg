1e771347bba54c239a7074bfa2a22298cfe631f6
Iker <xshine555@gmail.com>
2024-10-18 13:20:19 +0200

    Vale

--- 12_12_trobaproxim/TrobaMesProxim.java
+++ 12_12_trobaproxim/TrobaMesProxim.java
@@ -7,20 +7,12 @@ public class TrobaMesProxim {
 		}
 		else {
 			int valor = 0;
-			int prox = -1;
+			int prox = 0;
 			while (valor >= 0) {
 				System.out.println("Introdueix un valor");
 				valor = Integer.parseInt(Entrada.readLine());
 
-				if (valor < 0 && prox == -1) {
-					System.out.println("No s'ha introduït cap valor positiu");
-				}
-				if (prox == -1) {
-					prox = valor;
-				}
-				else if ( (Math.abs(ref-valor) < prox) && valor >= 0 ) {
-					prox = valor;
-				}
+				
 			}
 			System.out.println("El valor introduït més pròxim a " + ref + " és " + prox);
 		}
