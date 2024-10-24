fa81321ed912b42677ba1b196f1c40942e650228
prgtest <xshine555@gmail.com>
2024-10-24 09:42:20 +0200

    Naturals  ∾ prgtest autocommit 13_05 #1

--- 13_05_for_naturals/Naturals.java
+++ 13_05_for_naturals/Naturals.java
@@ -1,8 +1,13 @@
 public class Naturals {
 	public static void main(String[] args) {
 		int num = Integer.parseInt(args[0]);
-		for (int i = 1; i <= num; i++) {
-			System.out.println(i);
+		if (num < 1) {
+			System.out.format("Cap valor creixent entre 1 i %s", num);
+		}
+		else {
+			for (int i = 1; i <= num; i++) {
+				System.out.println(i);
+			}
 		}
 	}
 }
