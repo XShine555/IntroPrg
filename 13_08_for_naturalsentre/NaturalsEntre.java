040381b80890e952fbae2c6b73181d5f0f860306
Iker <xshine555@gmail.com>
2024-10-24 10:20:24 +0200

    A

--- 13_08_for_naturalsentre/NaturalsEntre.java
+++ 13_08_for_naturalsentre/NaturalsEntre.java
@@ -3,14 +3,29 @@ public class NaturalsEntre {
 		System.out.println("Valor inicial?");
 		int numInicial = Integer.parseInt(Entrada.readLine());
 
-		System.out.println("Valor final?");
-		int numFinal = Integer.parseInt(Entrada.readLine());
+		if (numInicial < 0) {
+			System.out.println("Valor no vàlid");
+		}
+		else {
+			System.out.println("Valor final?");
+			int numFinal = Integer.parseInt(Entrada.readLine());
 
-		System.out.println("Salt?");
-		int numSalto = Integer.parseInt(Entrada.readLine());
+			if (numFinal <= 0 || numInicial > numFinal) {
+				System.out.println("Valor no vàlid");
+			}
+			else {
+				System.out.println("Salt?");
+				int numSalto = Integer.parseInt(Entrada.readLine());
 
-		for (int i = numInicial; i <= numFinal; i += numSalto) {
-			System.out.println(i);
+				if (numSalto <= 0) {
+					System.out.println("Valor no vàlid");
+				}
+				else {
+					for (int i = numInicial; i <= numFinal; i += numSalto) {
+						System.out.println(i);
+					}
+				}
+			}
 		}
 	}
 }
