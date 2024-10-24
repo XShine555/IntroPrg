f88f5414e2a35b85c57129d1199d7cded9208179
Iker <xshine555@gmail.com>
2024-10-24 09:30:30 +0200

    NaturalsEntre

---
+++ 13_04_while_naturalsentre/NaturalsEntre.java
@@ -0,0 +1,17 @@
+public class NaturalsEntre {
+	public static void main(String[] args) {
+		System.out.println("Valor inicial?");
+		int numInicial = Integer.parseInt(Entrada.readLine());
+		System.out.println("Valor final?");
+		int numFinal = Integer.parseInt(Entrada.readLine());
+		System.out.println("Salt?");
+		int numSalto = Integer.parseInt(Entrada.readLine());
+
+		int currentNum = 0;
+
+		while (currentNum + numSalto < numFinal) {
+			currentNum += numSalto;
+			System.out.println(currentNum);
+		}
+	}
+}
