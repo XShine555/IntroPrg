539d97c9af987484ef8182a10332dcbf84c1282b
prgtest <xshine555@gmail.com>
2024-10-24 10:15:24 +0200

    NaturalsEntre  ∾ prgtest autocommit 13_04 #4

--- 13_04_while_naturalsentre/NaturalsEntre.java
+++ 13_04_while_naturalsentre/NaturalsEntre.java
@@ -3,19 +3,24 @@ public class NaturalsEntre {
 		System.out.println("Valor inicial?");
 		int numInicial = Integer.parseInt(Entrada.readLine());
 
-		System.out.println("Valor final?");
-		int numFinal = Integer.parseInt(Entrada.readLine());
-
-		System.out.println("Salt?");
-		int numSalto = Integer.parseInt(Entrada.readLine());
-
-		if (numInicial < 0 || numInicial < 0 || numFinal < 0 || numInicial > numFinal ) {
+		if (numInicial < 0) {
 			System.out.println("Valor no vàlid");
 		}
 		else {
-			while (numInicial <= numFinal) {
-				System.out.println(numInicial);
-				numInicial += numSalto;
+			System.out.println("Valor final?");
+			int numFinal = Integer.parseInt(Entrada.readLine());
+
+			System.out.println("Salt?");
+			int numSalto = Integer.parseInt(Entrada.readLine());
+
+			if (numSalto < 0 || numFinal < 0 || numInicial > numFinal ) {
+				System.out.println("Valor no vàlid");
+			}
+			else {
+				while (numInicial <= numFinal) {
+					System.out.println(numInicial);
+					numInicial += numSalto;
+				}
 			}
 		}
 	}
