e0f01a642dfc79e5bead9c7fbcb35ea70b9684a8
Iker <xshine555@gmail.com>
2024-10-24 09:45:48 +0200

    A

---
+++ 13_07_for_naturalspar/NaturalsParells.java
@@ -0,0 +1,13 @@
+public class NaturalsParells {
+	public static void main(String[] args) {
+		int value = Integer.parseInt(args[0]);
+		if (value < 1) {
+			System.out.format("Cap valor parell creixent entre 1 i %s", value);
+		}
+		else if (value % 2 == 0) {
+			for (int i = 0; i < value; i += 2) {
+				System.out.println(i);
+			}
+		}
+	}
+}
