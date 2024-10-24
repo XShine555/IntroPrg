8ca0ecc0491597e8b6ccbda812fb172495065616
prgtest <xshine555@gmail.com>
2024-10-24 10:58:59 +0200

    Hola  ∾ prgtest autocommit 13_11 #8

--- 13_11_edats/HistoricEdats.java
+++ 13_11_edats/HistoricEdats.java
@@ -1,35 +1,29 @@
 public class HistoricEdats {
-	public static void main(String[] args) {
-		System.out.println("nom?");
-		String nom = Entrada.readLine();
+    public static void main(String[] args) {
+        System.out.println("nom?");
+        String nom = Entrada.readLine();
 
-		if (nom.isBlank()) {
-			System.out.println("Entrada incorrecta");
-		}
-		else {
-			System.out.println("edat?");
-			int edat = Integer.parseInt(Entrada.readLine());
+        System.out.println("edat?");
+        int edat = Integer.parseInt(Entrada.readLine());
 
-			
-				System.out.println("any actual?");
-				int any = Integer.parseInt(Entrada.readLine());
+        System.out.println("any actual?");
+        int any = Integer.parseInt(Entrada.readLine());
 
-				if (any < 1971 || edat < 0) {
-					System.out.println("Entrada incorrecta");
-				} else {
-					for (int i = edat; i > 0; i--) {
-						int thisAny = any - i;
-						int thisEdat = edat - (any - thisAny);
-
-						String message = switch (thisEdat) {
-							case 0 -> String.format("El %s vau néixer", thisAny);
-							case 1 -> String.format("El %s teníeu %s any", thisAny, thisEdat);
-							default -> String.format("El %s teníeu %s anys", thisAny, thisEdat);
-						};
-						System.out.println(message);
-					}
-					System.out.println("Adéu " + nom);
-				}
-			}
-		}
-	}
+        if (any < 1971 || edat < 0 || nom.isBlank()) {
+                System.out.println("Entrada incorrecta");
+        }
+        else {
+                for (int i = edat; i > 0; i--) {
+                        int thisAny = any - i;
+                        int thisEdat = edat - (any - thisAny);
+                        String message = switch (thisEdat) {
+                                case 0 -> String.format("El %s vau néixer", thisAny);
+                                case 1 -> String.format("El %s teníeu %s any", thisAny, thisEdat);
+                                default -> String.format("El %s teníeu %s anys", thisAny, thisEdat);
+                        };
+                        System.out.println(message);
+                }
+                System.out.println("Adéu " + nom);
+        }
+    }
+}
