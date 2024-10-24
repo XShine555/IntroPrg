73b811132913d1fef09092e97b29a79e94864dfd
prgtest <xshine555@gmail.com>
2024-10-24 09:39:19 +0200

    NaturalsEntre  ∾ prgtest autocommit 13_04 #11

--- 13_04_while_naturalsentre/NaturalsEntre.java
+++ 13_04_while_naturalsentre/NaturalsEntre.java
@@ -1,11 +1,11 @@
 public class NaturalsEntre {
 	public static void main(String[] args) {
 		System.out.println("Valor inicial?");
-		String stringInicial = Entrada.readLine();
-		int numInicial = Integer.parseInt(stringInicial);
+		int numInicial = Integer.parseInt(Entrada.readLine());
+
 		System.out.println("Valor final?");
-		String stringFinal = Entrada.readLine();
-		int numFinal = Integer.parseInt(stringFinal);
+		int numFinal = Integer.parseInt(Entrada.readLine());
+
 		System.out.println("Salt?");
 		int numSalto = Integer.parseInt(Entrada.readLine());
 
