7c0fcc94645fcbce806a70fe55011f401ade7ab0
prgtest <xshine555@gmail.com>
2024-10-24 09:31:53 +0200

    NaturalsEntre  ∾ prgtest autocommit 13_04 #3

--- 13_04_while_naturalsentre/NaturalsEntre.java
+++ 13_04_while_naturalsentre/NaturalsEntre.java
@@ -9,7 +9,7 @@ public class NaturalsEntre {
 
 		int currentNum = 0;
 
-		while (currentNum + numSalto < numFinal) {
+		while (currentNum < numFinal) {
 			currentNum += numSalto;
 			System.out.println(currentNum);
 		}
