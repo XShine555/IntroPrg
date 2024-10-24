404e505de745eb5f1ea97c175d91812aaf847da5
Iker <xshine555@gmail.com>
2024-10-24 10:49:23 +0200

    Hola

---
+++ 13_11_edats/HistoricEdats.java
@@ -0,0 +1,39 @@
+public class HistoricEdats {
+	public static void main(String[] args) {
+		System.out.println("nom?");
+		String nom = Entrada.readLine();
+
+		if (nom.isBlank()) {
+			System.out.println("Entrada incorrecta");
+		}
+		else {
+			System.out.println("edat?");
+			int edat = Integer.parseInt(Entrada.readLine());
+
+			if (edat < 0) {
+				System.out.println("Entrada incorrecta");
+			}
+			else {
+				System.out.println("any actual?");
+				int any = Integer.parseInt(Entrada.readLine());
+
+				if (any < 1971) {
+					System.out.println("Entrada incorrecta");
+				} else {
+					for (int i = edat; i > 0; i--) {
+						int thisAny = any - i;
+						int thisEdat = edat - (any - thisAny);
+
+						String message = switch (thisEdat) {
+							case 0 -> String.format("El %s vau néixer", thisAny);
+							case 1 -> String.format("El %s tenieu %s any", thisAny, thisEdat);
+							default -> String.format("El %s tenieu %s any", thisAny, thisEdat);
+						};
+						System.out.println(message);
+					}
+					System.out.println("Adéu " + nom);
+				}
+			}
+		}
+	}
+}
