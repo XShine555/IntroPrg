0be2acaf578cd1d8b219ce879eec653a104015cb
Iker <xshine555@gmail.com>
2024-10-25 10:45:39 +0200

    Comentario

--- 13_10_mostrainterval/MostraInterval.java
+++ 13_10_mostrainterval/MostraInterval.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 13_10. Enters dins d'un interval
+ * Iker Rivera Garcia
+ * 25/10/2024
+ * Programa que pedirá dos número y si el número 1 es más grande que el segundo contara del número 1 al número 2 y si no al revés.
+*/
+
 public class MostraInterval {
 	public static void main(String[] args) {
 		System.out.println("inici?");
