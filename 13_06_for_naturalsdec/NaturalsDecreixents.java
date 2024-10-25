2db04267e3c6dd069641c1fb8999fb04e4dff3a6
Iker <xshine555@gmail.com>
2024-10-25 10:37:18 +0200

    Comentario

--- 13_06_for_naturalsdec/NaturalsDecreixents.java
+++ 13_06_for_naturalsdec/NaturalsDecreixents.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 13_06. Els primers naturals decreixentment novament
+ * Iker Rivera Garcia
+ * 25/10/2024
+ * Cuenta del 10 al 1 con un bucle for.
+*/
+
 public class NaturalsDecreixents {
 	public static void main(String[] args) {
 		int value = Integer.parseInt(args[0]);
