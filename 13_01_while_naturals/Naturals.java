2a33209404dcbf46e869f554a5494a8a2edcb70b
Iker <xshine555@gmail.com>
2024-10-25 10:28:53 +0200

    Comentario

--- 13_01_while_naturals/Naturals.java
+++ 13_01_while_naturals/Naturals.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 13_01. Els primers naturals
+ * Iker Rivera Garcia
+ * 25/10/2024
+ * Programa que cuenta del 1 al 10 usando un while.
+*/
+
 public class Naturals {
 	public static void main(String[] args) {
 		int count = 0, value = Integer.parseInt(args[0]);
