085be40623a516ec01fe89b25d03bb8a0d8c23ee
Iker <xshine555@gmail.com>
2024-10-25 10:40:00 +0200

    Comentario

--- 13_08_for_naturalsentre/NaturalsEntre.java
+++ 13_08_for_naturalsentre/NaturalsEntre.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 13_08. Naturals de principi a fi novament
+ * Iker Rivera Garcia
+ * 25/10/2024
+ * Programa que cuenta de X a X sumando X
+*/
+
 public class NaturalsEntre {
 	public static void main(String[] args) {
 		System.out.println("Valor inicial?");
