95cf0e9ef61f7d0f1f12fe8417c07b860868658e
Iker <xshine555@gmail.com>
2024-10-25 10:38:34 +0200

    Comentario

--- 13_07_for_naturalspar/NaturalsParells.java
+++ 13_07_for_naturalspar/NaturalsParells.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 13_07. Els primers parells novament
+ * Iker Rivera Garcia
+ * 25/10/2024
+ * Programa que cuenta del 0 al X con números par.
+*/
+
 public class NaturalsParells {
 	public static void main(String[] args) {
 		int value = Integer.parseInt(args[0]);
