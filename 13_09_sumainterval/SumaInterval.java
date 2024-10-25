ceef2b984a562284e3da79b08f7fa6d34ee10f8f
Iker <xshine555@gmail.com>
2024-10-25 10:41:20 +0200

    Comentario

--- 13_09_sumainterval/SumaInterval.java
+++ 13_09_sumainterval/SumaInterval.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 13_09. Suma valors entre dos nombres
+ * Iker Rivera Garcia
+ * 25/10/2024
+ * Programa que le pedirá dos valores al usuario y los irá sumando hasta llegar al valor más alto introducido.
+*/
+
 public class SumaInterval {
 	public static void main(String[] args) {
 		System.out.println("primer?");
