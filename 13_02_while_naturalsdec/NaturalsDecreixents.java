9adb741e5183b899deb5fbc0b694d314e0e341f1
Iker <xshine555@gmail.com>
2024-10-25 10:30:33 +0200

    Comentario

--- 13_02_while_naturalsdec/NaturalsDecreixents.java
+++ 13_02_while_naturalsdec/NaturalsDecreixents.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 13_02. Els primers naturals
+ * Iker Rivera Garcia
+ * 25/10/2024
+ * Programa que cuenta del 10 al 1.
+*/
+
 public class NaturalsDecreixents {
 	public static void main(String[] args) {
 		int value = Integer.parseInt(args[0]);
