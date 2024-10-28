d59fc0acad8460e0730fca9982370f3b81d784e0
Iker <xshine555@gmail.com>
2024-10-28 13:29:01 +0100

    Ndea

---
+++ 13_15_triangles/Triangle.java
@@ -0,0 +1,31 @@
+public class Triangle {
+	public static void main(String[] args) {
+		int length = 19, height = 10;
+
+		int mid = (length / 2);
+
+		System.out.println("quants?");
+		int value = Integer.parseInt(Entrada.readLine());
+
+		for (int triangle = 0; triangle < value; triangle++) {
+
+			for (int col = 0; col < height; col++) {
+
+				int dotsLeft = mid - col;
+				int dotsRight =  mid + col;
+
+				for (int row = 0; row < length; row++) {
+
+					if (row < dotsLeft || row > dotsRight) {
+						System.out.print(".");
+					}
+					else {
+						int num = row - dotsLeft;
+
+					}
+				}
+				System.out.println();
+			}
+		}
+	}
+}
