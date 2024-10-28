d991933176da2a9be38aaf0a7e93c5c945fd2cda
Iker <xshine555@gmail.com>
2024-10-28 12:05:28 +0100

    Hola

---
+++ 13_14_triangleinvertit/TriangleInvertit.java
@@ -0,0 +1,18 @@
+public class TriangleInvertit {
+	public static void main(String[] args) {
+		System.out.println("Nombre?");
+		int value = Integer.parseInt(Entrada.readLine());
+
+		for (int i = value; i > 0; i--) {
+			for (int j = 1; j <= i; j++) {
+				if (j == i) {
+					System.out.print(j);
+				}
+				else {
+					System.out.print(j + ", ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
