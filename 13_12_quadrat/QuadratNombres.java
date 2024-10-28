8d386442fe6b93150ce5398a83f6cff97d38bde4
Iker <xshine555@gmail.com>
2024-10-28 11:48:24 +0100

    Que?

---
+++ 13_12_quadrat/QuadratNombres.java
@@ -0,0 +1,20 @@
+/*
+ * Exercici 13_12. Un quadrat de nombres
+ * Iker Rivera Garcia
+ * 28/10/2024
+ * Programa que pide un número del 1 al 9 y dibuja un cuadrado.
+*/
+
+public class QuadratNombres {
+	public static void main(String[] args) {
+		System.out.println("Valor final?");
+		int value = Integer.parseInt(Entrada.readLine());
+
+		for (int i = 0; i < value; i++) {
+			for (int j = 1; j <= value; j++) {
+				System.out.print(" " + j + " ");
+			}
+			System.out.println();
+		}
+	}
+}
