4575b3d7612451b2d982b100ae716d54119bc9bc
Iker <xshine555@gmail.com>
2024-10-28 11:35:33 +0100

    Cambios

--- 12_06_trobamaxim/TrobaMaxim.java
+++ 12_06_trobamaxim/TrobaMaxim.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 12_06. Troba el màxim
+ * Iker Rivera Garcia
+ * 28/10/2024
+ * Programa que pide números positivos hasta que introduces uno negativo y mostrara el número introducido positivo más grande
+*/
+
 public class TrobaMaxim {
 	public static void main(String[] args) {
 		int maxValue = 0;
