4ca1e178a83e49c1fafb6d68e761cabff670112b
prgtest <xshine555@gmail.com>
2024-10-28 12:06:23 +0100

    Hola  ∾ prgtest autocommit 13_14 #1

--- 13_14_triangleinvertit/TriangleInvertit.java
+++ 13_14_triangleinvertit/TriangleInvertit.java
@@ -3,16 +3,21 @@ public class TriangleInvertit {
 		System.out.println("Nombre?");
 		int value = Integer.parseInt(Entrada.readLine());
 
-		for (int i = value; i > 0; i--) {
-			for (int j = 1; j <= i; j++) {
-				if (j == i) {
-					System.out.print(j);
+		if (value < 1 || value > 9) {
+			System.out.println("Valor inadequat");
+		}
+		else {
+			for (int i = value; i > 0; i--) {
+				for (int j = 1; j <= i; j++) {
+					if (j == i) {
+						System.out.print(j);
+					}
+					else {
+						System.out.print(j + ", ");
+					}
 				}
-				else {
-					System.out.print(j + ", ");
-				}
-			}
 			System.out.println();
+			}
 		}
 	}
 }
