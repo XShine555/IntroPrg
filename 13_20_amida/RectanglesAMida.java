33cf356cbc940563ba067c04d47e3420d9155d0a
prgtest <xshine555@gmail.com>
2024-11-06 09:49:29 +0100

    Uepa  ∾ prgtest autocommit 13_20 #2

--- 13_20_amida/RectanglesAMida.java
+++ 13_20_amida/RectanglesAMida.java
@@ -1,38 +1,44 @@
 public class RectanglesAMida {
 	public static void main(String[] args) {
 		int firstNumber = 1;
+		int secondNumber = -1;
 
 		int countRect = 0;
 		int punts = 0;
 
-		System.out.println(firstNumber + " x ?");
-		int secondNumber = Integer.parseInt(Entrada.readLine());
+		String inputString;
 
-		while (secondNumber > 0) {
-			countRect++;
-
-			for (int y = -1; y < firstNumber; y++) {
-				for (int x = -1; x < secondNumber; x++) {
-					if (y == -1) {
-						if (x == -1) { System.out.print("  "); }
-						else { System.out.print(x % 10); }
-					}
-					else {
-						if (x == -1) { System.out.print(y % 10 + " "); }
-						else
-						{
-							System.out.print("*");
-							punts++;
-							}
-					}
-				}
-				System.out.println();
-			}
-			firstNumber = secondNumber;
-
+		do {
 			System.out.println(firstNumber + " x ?");
-			secondNumber = Integer.parseInt(Entrada.readLine());
+			inputString = Entrada.readLine();
+			if (!inputString.isBlank()) {
+				secondNumber = Integer.parseInt(inputString);
+				if (secondNumber > 0) {
+					countRect++;
+
+					for (int y = -1; y < firstNumber; y++) {
+						for (int x = -1; x < secondNumber; x++) {
+							if (y == -1) {
+								if (x == -1) { System.out.print("  "); }
+								else { System.out.print(x % 10); }
+							}
+							else {
+								if (x == -1) { System.out.print(y % 10 + " "); }
+								else
+								{
+									System.out.print("*");
+									punts++;
+								}
+							}
+						}
+						System.out.println();
+					}
+					firstNumber = secondNumber;
+				}
+			}
 		}
+		while (secondNumber > 0 && !inputString.isBlank());
+
 		String countRectMsg = countRect > 0 ? "Rectangles: " + countRect : "Cap rectangle dibuixat";
 
 		System.out.println(countRectMsg);
