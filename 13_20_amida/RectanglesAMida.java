cc121f58b4ab113b6dd681c7fa733498fdd6a32b
Iker <xshine555@gmail.com>
2024-11-06 09:39:27 +0100

    Uepa

---
+++ 13_20_amida/RectanglesAMida.java
@@ -0,0 +1,35 @@
+public class RectanglesAMida {
+	public static void main(String[] args) {
+		int firstNumber = 1;
+
+		int countRect = 0;
+
+		System.out.println(firstNumber + " x ?");
+		int secondNumber = Integer.parseInt(Entrada.readLine());
+
+		while (secondNumber > 0) {
+			countRect++;
+
+			for (int y = -1; y < firstNumber; y++) {
+				for (int x = -1; x < secondNumber; x++) {
+					if (y == -1) {
+						if (x == -1) { System.out.print("  "); }
+						else { System.out.print(x % 10); }
+					}
+					else {
+						if (x == -1) { System.out.print(y % 10 + " "); }
+						else { System.out.print("*"); }
+					}
+				}
+				System.out.println();
+			}
+			firstNumber = secondNumber;
+
+			System.out.println(firstNumber + " x ?");
+			secondNumber = Integer.parseInt(Entrada.readLine());
+		}
+		String countRectMsg = countRect > 0 ? "Rectangles: " + countRect : "Cap rectangle dibuixat";
+
+		System.out.println(countRectMsg);
+	}
+}
