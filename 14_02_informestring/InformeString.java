9713ad9f4b96aedec0f679eaa090165d5b850740
prgtest <xshine555@gmail.com>
2024-11-08 12:28:50 +0100

    Terminado  ∾ prgtest autocommit 14_02 #1

--- 14_02_informestring/InformeString.java
+++ 14_02_informestring/InformeString.java
@@ -9,17 +9,17 @@ public class InformeString {
 		System.out.println("Número positiu?");
 		int numPos = Integer.parseInt(Entrada.readLine());
 
-		System.out.format("\"%s\".length(): %s %n", textoPrincipal, textoPrincipal.length());
-		System.out.format("\"%s\".startsWith(\"%s\"): %s %n", textoPrincipal, textoSecundario, textoPrincipal.startsWith(textoSecundario));
-		System.out.format("\"%s\".endsWith(\"%s\"): %s %n", textoPrincipal, textoSecundario, textoPrincipal.endsWith(textoSecundario));
-		System.out.format("\"%s\".equals(\"%s\"): %s %n", textoPrincipal, textoSecundario, textoPrincipal.equals(textoSecundario));
-		System.out.format("\"%s\".equalsIgnoreCase(\"%s\"): %s %n", textoPrincipal, textoSecundario, textoPrincipal.equalsIgnoreCase(textoSecundario));
-		System.out.format("\"%s\".isBlank(): %s %n", textoPrincipal, textoPrincipal.isBlank());
-		System.out.format("\"%s\".isEmpty(): %s %n", textoPrincipal, textoPrincipal.isEmpty());
-		System.out.format("\"%s\".charAt(%s): %s %n", textoPrincipal, numPos, textoPrincipal.charAt(numPos));
-		System.out.format("\"%s\".concat(\"%s\"): %s %n", textoPrincipal, textoSecundario, textoPrincipal.concat(textoSecundario));
-		System.out.format("\"%s\".repeat(%s): %s %n", textoPrincipal, numPos, textoPrincipal.repeat(numPos));
-		System.out.format("\"%s\".toUpperCase(): %s %n", textoPrincipal, textoPrincipal.toUpperCase());
-		System.out.format("\"%s\".toLowerCase(): %s %n", textoPrincipal, textoPrincipal.toLowerCase());
+		System.out.format("\"%s\".length(): %s%n", textoPrincipal, textoPrincipal.length());
+		System.out.format("\"%s\".startsWith(\"%s\"): %s%n", textoPrincipal, textoSecundario, textoPrincipal.startsWith(textoSecundario));
+		System.out.format("\"%s\".endsWith(\"%s\"): %s%n", textoPrincipal, textoSecundario, textoPrincipal.endsWith(textoSecundario));
+		System.out.format("\"%s\".equals(\"%s\"): %s%n", textoPrincipal, textoSecundario, textoPrincipal.equals(textoSecundario));
+		System.out.format("\"%s\".equalsIgnoreCase(\"%s\"): %s%n", textoPrincipal, textoSecundario, textoPrincipal.equalsIgnoreCase(textoSecundario));
+		System.out.format("\"%s\".isBlank(): %s%n", textoPrincipal, textoPrincipal.isBlank());
+		System.out.format("\"%s\".isEmpty(): %s%n", textoPrincipal, textoPrincipal.isEmpty());
+		System.out.format("\"%s\".charAt(%s): %s%n", textoPrincipal, numPos, textoPrincipal.charAt(numPos));
+		System.out.format("\"%s\".concat(\"%s\"): %s%n", textoPrincipal, textoSecundario, textoPrincipal.concat(textoSecundario));
+		System.out.format("\"%s\".repeat(%s): %s%n", textoPrincipal, numPos, textoPrincipal.repeat(numPos));
+		System.out.format("\"%s\".toUpperCase(): %s%n", textoPrincipal, textoPrincipal.toUpperCase());
+		System.out.format("\"%s\".toLowerCase(): %s%n", textoPrincipal, textoPrincipal.toLowerCase());
 	}
 }
