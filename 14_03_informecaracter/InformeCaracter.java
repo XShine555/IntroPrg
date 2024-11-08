b54e01f6da6eb8191d8600c09aaf231249d6eb00
prgtest <xshine555@gmail.com>
2024-11-08 12:43:23 +0100

    Char  ∾ prgtest autocommit 14_03 #4

--- 14_03_informecaracter/InformeCaracter.java
+++ 14_03_informecaracter/InformeCaracter.java
@@ -10,7 +10,7 @@ public class InformeCaracter {
 			System.out.println("Error");
 		}
 		else {
-			char character = text.charAt(pos);
+			char character = text.charAt(pos - 1);
 
 			System.out.format("Character.getName('%s'): %s%n", character, Character.getName(character));
 			System.out.format("Character.isDigit('%s'): %s%n", character, Character.isDigit(character));
