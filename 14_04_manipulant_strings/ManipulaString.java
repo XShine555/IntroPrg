85892be7b3f65499585146ef99d0892c3f3f1625
prgtest <xshine555@gmail.com>
2024-11-11 13:08:04 +0100

    2^32  ∾ prgtest autocommit 14_04 #5

--- 14_04_manipulant_strings/ManipulaString.java
+++ 14_04_manipulant_strings/ManipulaString.java
@@ -28,7 +28,7 @@ public class ManipulaString {
 			// Una composició formada per la primera lletra de la paraula en
        			// majúscules, seguida de les següents tres lletres en minúscules. Per
 		 	// exemple, si la paraula és «nEkAnE», resultatà en «Neka».
-			String firstFour = paraula.substring(0, 1).toUpperCase() + paraula.substring(1, 3).toLowerCase();
+			String firstFour = paraula.substring(0, 1).toUpperCase() + paraula.substring(1, 4).toLowerCase();
 	        	System.out.format("La composició de quatre primeres és \"%s\"%n", firstFour);
 
 	        	// Mostra una composició formada per la darrera lletra de la paraula,
