d59b584533b47494b8c8b6b72f2a3b7b8f6a5498
prgtest <xshine555@gmail.com>
2024-11-11 12:44:23 +0100

    2^32  ∾ prgtest autocommit 14_04 #1

--- 14_04_manipulant_strings/ManipulaString.java
+++ 14_04_manipulant_strings/ManipulaString.java
@@ -7,7 +7,7 @@ public class ManipulaString {
         String paraula = Entrada.readLine();
 
         // Mostra la longitud de la paraula (quantes lletres conté)
-        System.out.println("La longitud és: " + paraula.length());
+        System.out.println("La longitud és " + paraula.length());
 
         // Mostra la segona lletra de la paraula
         System.out.format("La segona lletra és '%s'%n", paraula.charAt(1));
