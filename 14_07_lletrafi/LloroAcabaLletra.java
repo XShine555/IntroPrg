6ae12eb9edec919d747bcce8287686529976dcbc
Iker <xshine555@gmail.com>
2024-11-14 12:17:56 +0100

    Suele pasar

--- 14_07_lletrafi/LloroAcabaLletra.java
+++ 14_07_lletrafi/LloroAcabaLletra.java
@@ -1,13 +1,13 @@
-public class LloroVocalIniFi {
+public class LloroAcabaLletra  {
 	public static void main(String[] args) {
                 System.out.println("Paraula?");
                 String userInput = Entrada.readLine();
 
                 while (!userInput.isBlank()) {
 			String upper = userInput.toUpperCase();
-			char vocal = upper.charAt(0);
+			char vocal = upper.charAt(upper.length()-1);
 			boolean esVocal = vocal == 'A' || vocal == 'E' || vocal == 'I' || vocal == 'O' || vocal == 'U';
-			if (esVocal && upper.endsWith(String.valueOf(vocal))) {
+			if (!esVocal && upper.endsWith(String.valueOf(vocal))) {
 				System.out.format("Repeteixo: %s%n", userInput);
 			}
 
