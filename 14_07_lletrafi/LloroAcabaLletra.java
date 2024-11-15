6375499b4b3d9dc8a003aaeca4a2d75386e247da
prgtest <xshine555@gmail.com>
2024-11-15 11:58:21 +0100

    Comentarios  ∾ prgtest autocommit 14_07 #1

--- 14_07_lletrafi/LloroAcabaLletra.java
+++ 14_07_lletrafi/LloroAcabaLletra.java
@@ -12,17 +12,10 @@ public class LloroAcabaLletra  {
 
                 while (!userInput.isBlank()) {
 			String upper = userInput.toUpperCase();
-			char vocal = 'a';
-			for (int i = upper.length()-1; i > 0; i--) {
-				char charAt = upper.charAt(i);
-				if (Character.isDigit(charAt)) {
-					vocal = charAt;
-					break;
-				}
-			}
-//			char vocal = upper.charAt(upper.length()-1);
-			boolean esVocal = vocal == 'A' || vocal == 'E' || vocal == 'I' || vocal == 'O' || vocal == 'U';
-			if (!esVocal && upper.endsWith(String.valueOf(vocal))) {
+			char lastChar = upper.charAt(upper.length() - 1);
+
+			boolean esVocal = lastChar == 'A' || lastChar == 'E' || lastChar == 'I' || lastChar == 'O' || lastChar == 'U';
+			if (!esVocal && upper.endsWith(String.valueOf(lastChar))) {
 				System.out.format("Repeteixo: %s%n", userInput);
 			}
 
