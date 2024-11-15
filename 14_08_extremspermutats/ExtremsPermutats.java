3b28bb900f697d8bcd3c1e9c47c7bc37087b9dd8
Iker <xshine555@gmail.com>
2024-11-15 12:38:04 +0100

    Uepa

---
+++ 14_08_extremspermutats/ExtremsPermutats.java
@@ -0,0 +1,30 @@
+/*
+ * Exercici 14_08. Extrems permutats
+ * Iker Rivera Garcia
+ * 15/11/2024
+ *
+*/
+
+public class ExtremsPermutats {
+	public static void main(String[] args) {
+		System.out.println("Ves introduïnt texts (finalitza amb INTRO sol)");
+		String userInput = Entrada.readLine();
+
+		while (!userInput.isBlank()) {
+			String firstSection = userInput.substring(0, 2);
+			String lastSection = userInput.substring(userInput.length() - 2);
+
+			String rotateLastSection = "";
+			for (int i = lastSection.length() - 1; i > -1; i--) {
+				rotateLastSection += lastSection.charAt(i);
+			}
+
+			if (firstSection.equals(lastSection) || firstSection.equals(rotateLastSection)) {
+				System.out.format("Repeteix: %s%n", userInput);
+			}
+
+			System.out.println("Ves introduïnt texts (finalitza amb INTRO sol)");
+			userInput = Entrada.readLine();
+		}
+	}
+}
