7349e623f9b83976a80fecdedb76a66abf4fcd85
prgtest <xshine555@gmail.com>
2024-11-15 12:26:26 +0100

    Comentarios  ∾ prgtest autocommit 14_07 #4

--- 14_07_lletrafi/LloroAcabaLletra.java
+++ 14_07_lletrafi/LloroAcabaLletra.java
@@ -12,19 +12,10 @@ public class LloroAcabaLletra  {
 
                 while (!userInput.isBlank()) {
 			String upper = userInput.toUpperCase();
-			char searchChar = Character.MIN_VALUE;
-			for (int i = upper.length() - 1; i > 0; i--) {
-				char thisChar = upper.charAt(i);
-				if (Character.isLetter(thisChar)) {
-					searchChar = thisChar;
-					break;
-				}
-			}
-			if (searchChar != Character.MIN_VALUE) {
-				String lastChar = String.valueOf(searchChar);
-				if (!lastChar.endsWith("A") && !lastChar.endsWith("E") && !lastChar.endsWith("I") && !lastChar.endsWith("O") && !lastChar.endsWith("U")) {
-					System.out.format("Repeteixo: %s%n", userInput);
-				}
+			char lastChar = upper.charAt(upper.length() - 1);
+
+			if (Character.isLetter(lastChar) && lastChar != 'A' && lastChar != 'E' && lastChar != 'I' && lastChar != 'O' && lastChar != 'U') {
+				System.out.format("Repeteixo: %s%n", userInput);
 			}
 
                         System.out.println("Paraula?");
