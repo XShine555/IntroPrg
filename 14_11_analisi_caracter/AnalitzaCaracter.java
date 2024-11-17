3720933402da774f208d8979aca5acfc922c5caf
Iker <xshine555@gmail.com>
2024-11-17 16:59:38 +0100

    Otro

---
+++ 14_11_analisi_caracter/AnalitzaCaracter.java
@@ -0,0 +1,34 @@
+public class AnalitzaCaracter {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+
+        System.out.println("Posició?");
+        int position = Integer.parseInt(Entrada.readLine());
+
+        if (text.isBlank()) {
+            System.out.println("Text buit");
+        } else {
+            int textLen = text.length();
+
+            if (position < 0) {
+                position = textLen + position;
+            } else if (position > textLen) {
+                position = position % textLen;
+            }
+
+            char selectedChar = text.charAt(position);
+
+            String message = String.format("'%s' és ", selectedChar);
+
+            if (Character.isLetter(selectedChar)) {
+                String mayusOrMinus = Character.isUpperCase(selectedChar) ? "majúscula" : "minúscula";
+                message += String.format("una lletra %s", mayusOrMinus);
+            } else {
+                message += String.format("un digit");
+            }
+
+            System.out.println(message);
+        }
+    }
+}
