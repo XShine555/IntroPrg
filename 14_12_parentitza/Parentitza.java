1657bb01ebf1869ae3f4b94ad9426f1428f927a3
prgtest <xshine555@gmail.com>
2024-11-17 17:31:03 +0100

    Otro  ∾ prgtest autocommit 14_12 #1

--- 14_12_parentitza/Parentitza.java
+++ 14_12_parentitza/Parentitza.java
@@ -6,7 +6,7 @@ public class Parentitza {
         for (int i = 0; i < text.length(); i++) {
             char iChar = text.charAt(i);
 
-            if (Character.isLetterOrDigit(iChar)) {
+            if (Character.isLetter(iChar)) {
                 System.out.format("(%s)", iChar);
             } else {
                 System.out.print(iChar);
