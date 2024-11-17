6dd090667478152899582024a9aef8ea615dd83a
prgtest <xshine555@gmail.com>
2024-11-17 18:06:40 +0100

    Epa  ∾ prgtest autocommit 14_14 #3

--- 14_14_distribuidor/Distribuidor.java
+++ 14_14_distribuidor/Distribuidor.java
@@ -45,7 +45,7 @@ public class Distribuidor {
             System.out.format("Nombres: %s%n", names);
         }
         if (symbols.length() > 0) {
-            System.out.format("Símbols: %s%n", symbols);
+            System.out.format("Simbols: %s%n", symbols);
         }
     }
 }
