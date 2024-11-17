b5722eb1502751b42ea34d2113d8fd1d6b367222
prgtest <xshine555@gmail.com>
2024-11-17 17:43:41 +0100

    Esto para mi es como cocinar ternera  ∾ prgtest autocommit 14_13 #3

--- 14_13_nomeslletres/NomesLletres.java
+++ 14_13_nomeslletres/NomesLletres.java
@@ -1,27 +1,20 @@
+import java.util.StringJoiner;
+
 public class NomesLletres {
     public static void main(String[] args) {
         System.out.println("Text?");
         String text = Entrada.readLine();
 
-        String sanitizeText = "";
+        StringJoiner finalText = new StringJoiner(", ");
 
         for (int i = 0; i < text.length(); i++) {
             char iChar = text.charAt(i);
 
-            if (Character.isLetter(iChar)){
-                sanitizeText += iChar;
+            if (Character.isLetter(iChar)) {
+                finalText.add(String.valueOf(iChar));
             }
         }
 
-        for (int i = 0; i < sanitizeText.length(); i++) {
-            char iChar = sanitizeText.charAt(i);
-
-            if (i < 1) {
-                System.out.print(iChar);
-            }
-            else {
-                System.out.print(", " + iChar);
-            }
-        }
+        System.out.println(finalText);
     }    
 }
