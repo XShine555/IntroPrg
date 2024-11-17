11792ce43eb4de01c348ba4da628e100e2be41d9
Iker <xshine555@gmail.com>
2024-11-17 17:38:47 +0100

    Esto para mi es como cocinar ternera

---
+++ 14_13_nomeslletres/NomesLletres.java
@@ -0,0 +1,19 @@
+public class NomesLletres {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+
+        for (int i = 0; i < text.length(); i++) {
+            char iChar = text.charAt(i);
+
+            if (Character.isLetter(iChar)) {
+                if (i == text.length() - 1) {
+                    System.out.print(iChar);
+                }
+                else{
+                    System.out.print(iChar + ", ");
+                }
+            }
+        }
+    }    
+}
