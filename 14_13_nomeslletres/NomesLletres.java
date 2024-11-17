dfa1854941b8e1de6b8f321aebee1ba2bade07a9
prgtest <xshine555@gmail.com>
2024-11-17 17:41:05 +0100

    Esto para mi es como cocinar ternera  ∾ prgtest autocommit 14_13 #2

--- 14_13_nomeslletres/NomesLletres.java
+++ 14_13_nomeslletres/NomesLletres.java
@@ -3,16 +3,24 @@ public class NomesLletres {
         System.out.println("Text?");
         String text = Entrada.readLine();
 
+        String sanitizeText = "";
+
         for (int i = 0; i < text.length(); i++) {
             char iChar = text.charAt(i);
 
-            if (Character.isLetter(iChar)) {
-                if (i < 1) {
-                    System.out.print(iChar);
-                }
-                else {
-                    System.out.print(", " + iChar);
-                }
+            if (Character.isLetter(iChar)){
+                sanitizeText += iChar;
+            }
+        }
+
+        for (int i = 0; i < sanitizeText.length(); i++) {
+            char iChar = sanitizeText.charAt(i);
+
+            if (i < 1) {
+                System.out.print(iChar);
+            }
+            else {
+                System.out.print(", " + iChar);
             }
         }
     }    
