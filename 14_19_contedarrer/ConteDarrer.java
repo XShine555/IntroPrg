483b524125d2aea7a06f082748c0d08aedef2796
prgtest <xshine555@gmail.com>
2024-11-18 13:17:16 +0100

    Init  ∾ prgtest autocommit 14_19 #1

--- 14_19_contedarrer/ConteDarrer.java
+++ 14_19_contedarrer/ConteDarrer.java
@@ -2,9 +2,30 @@ public class ConteDarrer {
     public static void main(String[] args) {
         System.out.println("Introdueix texts (enter sol per finalitzar)");
         String text = Entrada.readLine();
+        char toFind = Character.MAX_VALUE;
+        boolean canExit = false;
 
-        while (!text.isBlank()) {
-            
+        while (!text.isBlank() && !canExit) {
+            // Busca una letra mayúscula.
+            if (toFind == Character.MAX_VALUE) {
+                System.out.println("bé");
+            }
+            else {
+                boolean foundAnything = false;
+                for (int i = 0; i < text.length() && !foundAnything; i++) {
+                    char iChar = Character.toUpperCase(text.charAt(i));
+                    if (iChar == toFind) {
+                        foundAnything = true;
+                    }
+                    System.out.println(iChar);
+                }
+
+                canExit = !foundAnything;
+                if (foundAnything) {
+                    System.out.println("bé");
+                }
+            }
+            toFind = Character.toUpperCase(text.charAt(text.length() - 1));
 
             text = Entrada.readLine();
         }
