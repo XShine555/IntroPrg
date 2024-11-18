afd43f2f16c16c93863aa52163fe490882f6d4d7
Iker <xshine555@gmail.com>
2024-11-18 11:59:57 +0100

    TryCatch

--- 14_17_enterestricte/Enters.java
+++ 14_17_enterestricte/Enters.java
@@ -1,3 +1,34 @@
+/*
+ * Exercici 14_17. Enters estrictes
+ * Iker Rivera Garcia
+ * 18/11/2024
+ * Programa que indica si un texto es entero o no.
+ */
+
 public class Enters {
-    
+    public static void main(String[] args) {
+        System.out.println("Introdueix texts (enter sol per finalitzar)");
+        String text = Entrada.readLine();
+
+        while (!text.isBlank()) {
+            boolean isNumber = true;
+
+            for (int i = 0; i < text.length() && isNumber; i++) {
+                char iChar = text.charAt(i);
+                if (!Character.isDigit(iChar)) {
+                    isNumber = false;
+                }
+            }
+
+            if (isNumber) {
+                System.out.println("És enter");
+            } else {
+                System.out.println("No és enter");
+            }
+
+            text = Entrada.readLine();
+        }
+
+        System.out.println("Adéu");
+    }
 }
