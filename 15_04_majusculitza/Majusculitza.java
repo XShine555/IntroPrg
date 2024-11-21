59f43f817360026321a727bdb6dbc3cc3e80bf1e
Iker <xshine555@gmail.com>
2024-11-21 11:47:42 +0100

    Maj

---
+++ 15_04_majusculitza/Majusculitza.java
@@ -0,0 +1,28 @@
+public class Majusculitza {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String userInput = Entrada.readLine();
+        int textLen = userInput.length();
+
+        String newText = "";
+        for (int i = 0; i < textLen; i++) {
+            char iChar = userInput.charAt(i);
+
+            if (i > 0) {
+                char lastChar = userInput.charAt(i - 1);
+                if (Character.isWhitespace(lastChar)) {
+                    iChar = Character.toUpperCase(iChar);
+                }
+                else {
+                    iChar = Character.toLowerCase(iChar);
+                }
+            }
+            else {
+                iChar = Character.toUpperCase(iChar);
+            }
+
+            newText += iChar;
+        }
+        System.out.println(newText);
+    }    
+}
