5bd2e0e3d96ed9d9fe3db3d9becd7107ba3e62cf
Iker <xshine555@gmail.com>
2024-11-21 11:34:57 +0100

    Informe

---
+++ 15_03_informetext/InformeText.java
@@ -0,0 +1,59 @@
+public class InformeText {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String userInput = Entrada.readLine();
+        int textLen = userInput.length();
+
+        System.out.println("Informe");
+        System.out.println("=======");
+
+        int letterCount = 0, mayusCount = 0, minusCount = 0;
+        int vowelCount = 0, vowelMayusCount = 0, vowelMinusCount = 0;
+        int numberCount = 0, otherSymbols = 0;
+
+        for (int i = 0; i < textLen; i++) {
+            char iChar = userInput.charAt(i);
+
+            if (Character.isLetter(iChar)) {
+                letterCount++;
+
+                if (Character.isUpperCase(iChar)) {
+                    mayusCount++;
+
+                    if (iChar == 'a' || iChar == 'e' || iChar == 'i' || iChar == 'o'
+                            || iChar == 'u' || iChar == 'à' || iChar == 'é' || iChar == 'è'
+                            || iChar == 'í' || iChar == 'ï' || iChar == 'ó' || iChar == 'ò'
+                            || iChar == 'ú' || iChar == 'ü') {
+                        vowelMayusCount++;
+                        vowelCount++;
+                    }
+                } else {
+                    minusCount++;
+                    if (iChar == 'A' || iChar == 'E' || iChar == 'I' || iChar == 'O'
+                            || iChar == 'U' || iChar == 'À' || iChar == 'É' || iChar == 'È'
+                            || iChar == 'Í' || iChar == 'Ï' || iChar == 'Ó' || iChar == 'Ò'
+                            || iChar == 'Ú' || iChar == 'Ü') {
+                        vowelMayusCount++;
+                        vowelCount++;
+                    }
+                }
+            }
+            else if (Character.isDigit(iChar)) {
+                numberCount++;
+            }
+            else {
+                otherSymbols++;
+            }
+        }
+
+        System.out.format("lletres en majúscules: %d (%.2f%%)%n", mayusCount, 100.0 * mayusCount / textLen);
+        System.out.format("lletres en minúscules: %d (%.2f%%)%n", minusCount, 100.0 * minusCount / textLen);
+        System.out.format("total lletres: %d (%.2f%%)%n", letterCount, 100.0 * letterCount / textLen);
+        System.out.format("vocals en majúscules: %d (%.2f%%)%n", vowelMayusCount, 100.0 * vowelMayusCount / textLen);
+        System.out.format("vocals en minúscules: %d (%.2f%%)%n", vowelMinusCount, 100.0 * vowelMayusCount / textLen);
+        System.out.format("total vocals: %d (%.2f%%)%n", vowelCount, 100.0 * vowelCount / textLen);
+        System.out.format("digits: %d (%.2f%%)%n", numberCount, 100.0 * numberCount / textLen);
+        System.out.format("altres caràcters: %d (%.2f%%)%n", otherSymbols, 100.0 * otherSymbols / textLen);
+        System.out.format("total caràcters: %d", textLen);
+    }
+}
