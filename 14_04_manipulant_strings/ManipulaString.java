a8dbf6d2effd50458668e99297492c3b2a6bf3e2
prgtest <xshine555@gmail.com>
2024-11-21 10:15:23 +0100

    Substring  ∾ prgtest autocommit 14_04 #3

--- 14_04_manipulant_strings/ManipulaString.java
+++ 14_04_manipulant_strings/ManipulaString.java
@@ -49,7 +49,7 @@ public class ManipulaString {
 			// minúscules de manera que la primera sigui majúscules i la resta en
 			// minúscules. (ex. si la paraula és «Nekane» mostrarà «Enek»)
 			String firstThreeAndLastMayus = "" + Character.toUpperCase(paraula.charAt(0));
-			for (int i = 0; i < paraula.length(); i++) {
+			for (int i = 1; i < paraula.length(); i++) {
 				firstThreeAndLastMayus += Character.toLowerCase(paraula.charAt(i));
 			}
 			System.out.format("La composició de quatre corregint majúscules és \"%s\"%n", firstThreeAndLastMayus);
