1efad7b341848e4c7ea502f420be5ade41519638
Iker <xshine555@gmail.com>
2024-11-21 10:40:54 +0100

    Ej

---
+++ 15_01_vocalsmajuscules/VocalsMajuscules.java
@@ -0,0 +1,23 @@
+/*
+ * Exercici 15_01. Vocals en majúscules
+ * Iker Rivera Garcia
+ * 21/11/2024
+ * Programa que convertirá las vocales en mayúscula.
+ */
+
+public class VocalsMajuscules {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String userInput = Entrada.readLine();
+
+        String newText = "";
+        for (int i = 0; i < userInput.length(); i++) {
+            char iChar = userInput.charAt(i);
+            boolean isMayus = iChar == 'a' || iChar == 'e' || iChar == 'i' || iChar == 'o' || iChar == 'u';
+
+            newText += isMayus ? Character.toUpperCase(iChar) : iChar;
+        }
+
+        System.out.println(newText);
+    }
+}
