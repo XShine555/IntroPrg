273c4cb6645d3813d4b744c2825a47fd38f0f789
prgtest <xshine555@gmail.com>
2024-11-22 11:44:47 +0100

    First  ∾ prgtest autocommit 14_20 #7

--- 14_20_endevinacontrolat/EndevinaNombre.java
+++ 14_20_endevinacontrolat/EndevinaNombre.java
@@ -7,7 +7,7 @@ public class EndevinaNombre {
 
         boolean hasWin = false;
 
-        while (!userInput.isBlank() && !hasWin) {
+        while (!userInput.isBlank()) {
             boolean isNumber = true;
             for (int i = 0; i < userInput.length(); i++) {
                 char iChar = userInput.charAt(i);
@@ -29,6 +29,7 @@ public class EndevinaNombre {
                 }
                 else {
                     hasWin = true;
+                    break;
                 }
             }
             else {
