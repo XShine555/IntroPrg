5fed222119785287b5433dfb40069a2d68d60dda
prgtest <xshine555@gmail.com>
2024-11-28 10:24:26 +0100

    Capicua  ∾ prgtest autocommit 15_13 #11

--- 15_13_capicuarevisada/Capicua.java
+++ 15_13_capicuarevisada/Capicua.java
@@ -8,7 +8,7 @@ public class Capicua {
         for (int i = 0; i < inputLen; i++) {
             char iChar = userInput.charAt(i);
 
-            if (Character.isWhitespace(iChar) && !Character.isLetter(iChar) && !Character.isDigit(iChar))
+            if (Character.isWhitespace(iChar) || iChar == '.' || iChar == ',')
                 continue;
 
             char newChar = switch (iChar) {
@@ -39,6 +39,7 @@ public class Capicua {
     
                 if (firstChar != lastChar){
                     isValid = false;
+                    System.out.println(firstChar + " != " + lastChar);
                     break;
                 }
             }
