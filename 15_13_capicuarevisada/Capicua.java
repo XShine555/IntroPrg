ba9293f06e6dd2001d438e47b204b920c9a5b589
prgtest <xshine555@gmail.com>
2024-11-28 10:17:10 +0100

    Capicua  ∾ prgtest autocommit 15_13 #4

--- 15_13_capicuarevisada/Capicua.java
+++ 15_13_capicuarevisada/Capicua.java
@@ -8,6 +8,9 @@ public class Capicua {
         for (int i = 0; i < inputLen; i++) {
             char iChar = userInput.charAt(i);
 
+            if (!Character.isWhitespace(iChar))
+                continue;
+
             char newChar = switch (iChar) {
                 case 'à' -> 'a';
                 case 'é' -> 'e';
