d28c122cfdbd33a38a7d649c9dbda6da5e59f65d
prgtest <xshine555@gmail.com>
2024-11-28 11:37:50 +0100

    First  ∾ prgtest autocommit 15_14 #1

--- 15_14_paraulesreves/ParaulesReves.java
+++ 15_14_paraulesreves/ParaulesReves.java
@@ -3,8 +3,28 @@ public class ParaulesReves {
         System.out.println("Text?");
         String userInput = Entrada.readLine();
 
+        String result = "";
+        String tempString = "";
+
         for (int i = userInput.length() - 1; i > -1; i--) {
-            System.out.print(userInput.charAt(i));
+            char iChar = userInput.charAt(i);
+            if (i == 0) {
+                tempString += iChar;
+            }
+
+            if (Character.isWhitespace(iChar) || i == 0) {
+                String thisResult = "";
+                for (int j = 0; j < tempString.length(); j++) {
+                    thisResult += tempString.charAt(j);
+                }
+                tempString = "";
+                result = thisResult + " " + result;
+
+            } else {
+                tempString += iChar;
+            }
         }
+
+        System.err.println(result);
     }    
 }
