802ea1af7b2efe6641078e22612e68e77dd1aa8c
Iker <xshine555@gmail.com>
2024-12-02 08:33:58 +0100

    Comentario

--- 15_09_trianglelletres/TriangleLletres.java
+++ 15_09_trianglelletres/TriangleLletres.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 15_09. Un triangle de lletres
+ * Iker Rivera Garcia
+ * 2/12/2024
+ * Dibuja un triángulo con las letras de la frase.
+ */
+
 public class TriangleLletres {
     public static void main(String[] args) {
         System.out.println("Text?");
