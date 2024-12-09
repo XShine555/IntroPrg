10dcfea3ac710db37201984ba5d674f7765a7df6
Iker <xshine555@gmail.com>
2024-12-09 08:40:41 +0100

    Quad

---
+++ 16_02_quadrats_modul/Quadrats.java
@@ -0,0 +1,20 @@
+public class Quadrats {
+    public static void main(String[] args) {
+        dibuixaQuadrats();
+    }    
+
+    public static void dibuixaQuadrats() {
+        System.out.println("Quants?");
+        int amount = Integer.parseInt(Entrada.readLine());
+
+        for (int i = 0; i < amount; i++) {
+            for (int y = 0; y < 5; y++) {
+                for (int x = 0; x < 5; x++) {
+                    System.out.print(" X");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
