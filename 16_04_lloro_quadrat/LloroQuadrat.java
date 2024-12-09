8742d9c99cf37953ebb41ac7d30f95fccb091a47
prgtest <xshine555@gmail.com>
2024-12-09 09:03:12 +0100

    Uepa  ∾ prgtest autocommit 16_04 #2

--- 16_04_lloro_quadrat/LloroQuadrat.java
+++ 16_04_lloro_quadrat/LloroQuadrat.java
@@ -5,35 +5,45 @@ public class LloroQuadrat {
             System.out.println("El lloro espera paraula:");
             input = Entrada.readLine();
 
-            System.out.println(getResponse(input));
+            getResponse(input);
         }
         while (!input.isBlank());
 
         System.out.println("Adéu");
     }
 
-    public static String getResponse(String input) {
-        String response = switch (input)
-        {
-            case "dibuixa quadrat" -> drawCords(5, 5);
-            case "dibuixa rectangle" -> drawCords(10, 5);
-            default -> String.format("El lloro repeteix: %s", input);
-        };
-
-        return response;
+    public static void getResponse(String input) {
+        if (input.equals("dibuixa quadrat"))
+            dibuixaLiniaQuadrat();
+        else if (input.equals("dibuixa rectangle"))
+            dibuixaLiniaRectangle();
+        else
+            System.out.println(String.format("El lloro repeteix: %s", input));
     }
 
-    private static String drawCords(int xCoord, int yCoord) {
-        String result = "";
-
-        for (int y = 0; y < yCoord; y++) {
-            for (int x = 0; x < xCoord; x++) {
-                result += " X";
-            }
-            if (y != yCoord-1)
-                result += "\n";
+    public static void dibuixaQuadrat() {
+        for (int y=0; y < 5; y++) {
+            dibuixaLiniaQuadrat();
+            System.out.println();
         }
+    }
 
-        return result;
+    public static void dibuixaLiniaQuadrat() {
+        for (int x = 0; x < 5; x++) {
+            System.out.print(" X");
+        }
+    }
+
+    public static void dibuixaRectangle() {
+        for (int y=0; y < 5; y++) {
+            dibuixaLiniaQuadrat();
+            System.out.println();
+        }
+    }
+
+    public static void dibuixaLiniaRectangle() {
+        for (int x = 0; x < 5; x++) {
+            System.out.print(" X");
+        }
     }
 }
