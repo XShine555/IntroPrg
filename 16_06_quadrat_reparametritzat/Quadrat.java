8325bc52df1bab423d8b00bb5f06b61a63455a5f
prgtest <xshine555@gmail.com>
2024-12-09 09:10:40 +0100

    Ok  ∾ prgtest autocommit 16_06 #1

--- 16_06_quadrat_reparametritzat/Quadrat.java
+++ 16_06_quadrat_reparametritzat/Quadrat.java
@@ -1,18 +1,19 @@
 public class Quadrat {
     public static void main(String[] args) {
-        dibuixaQuadrat(Integer.parseInt(args[0]), args[1]);
+        if (args.length > 1 && args[1].length() > 0)
+            dibuixaQuadrat(Integer.parseInt(args[0]), args[1].charAt(0));
     }
 
-    public static void dibuixaQuadrat(int length, String ch) {
-        for (int y=0; y < length; y++) {
-            dibuixaLinia(length, ch);
+    public static void dibuixaQuadrat(int costat, char caracter) {
+        for (int y=0; y < costat; y++) {
+            dibuixaLinia(costat, caracter);
             System.out.println();
         }
     }
 
-    public static void dibuixaLinia(int length, String ch) {
-        for (int x = 0; x < length; x++) {
-            System.out.print(" " + ch);
+    public static void dibuixaLinia(int costat, char caracter) {
+        for (int x = 0; x < costat; x++) {
+            System.out.print(" " + caracter);
         }
     }
 }
