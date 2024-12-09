3f520e255d3c9e68e60cd64e4cef4f8105040585
Iker <xshine555@gmail.com>
2024-12-09 11:53:45 +0100

    Comentarios

--- 16_12_parentitza/Parentitza.java
+++ 16_12_parentitza/Parentitza.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 16_12. Lletres entre parèntesis
+ * Iker Rivera Garcia
+ * 09/12/2024
+ * Programa que pone las letras dentro de paréntesis.
+ */
+
 public class Parentitza {
     public static void main(String[] args) {
         System.out.println("Text?");
