9983aa4038d3814a93a652e06b8d1e6299cd74bb
Iker <xshine555@gmail.com>
2024-12-09 09:21:27 +0100

    ()

---
+++ 16_12_parentitza/Parentitza.java
@@ -0,0 +1,20 @@
+public class Parentitza {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+
+        parentitza(text);
+    }
+
+    public static void parentitza(String text) {
+        for (int i = 0; i < text.length(); i++) {
+            char iChar = text.charAt(i);
+
+            if (Character.isLetter(iChar)) {
+                System.out.format("(%s)", iChar);
+            } else {
+                System.out.print(iChar);
+            }
+        }
+    }
+}
