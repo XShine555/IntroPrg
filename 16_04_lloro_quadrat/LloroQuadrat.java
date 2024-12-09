ffc025e44549b8ce6be974415525e023cc012b88
Iker <xshine555@gmail.com>
2024-12-09 08:58:37 +0100

    Uepa

---
+++ 16_04_lloro_quadrat/LloroQuadrat.java
@@ -0,0 +1,37 @@
+public class LloroQuadrat {
+    public static void main(String[] args) {
+        String input = "";
+        do {
+            System.out.println("El lloro espera paraula:");
+            input = Entrada.readLine();
+
+            System.out.println(getResponse(input));
+        }
+        while (!input.isBlank());
+    }
+
+    public static String getResponse(String input) {
+        String response = switch (input)
+        {
+            case "dibuixa quadrat" -> drawCords(5, 5);
+            case "dibuixa rectangle" -> drawCords(10, 5);
+            default -> String.format("El lloro repeteix: %s", input);
+        };
+
+        return response;
+    }
+
+    private static String drawCords(int xCoord, int yCoord) {
+        String result = "";
+
+        for (int y = 0; y < yCoord; y++) {
+            for (int x = 0; x < xCoord; x++) {
+                result += " X";
+            }
+            if (y != yCoord-1)
+                result += "\n";
+        }
+
+        return result;
+    }
+}
