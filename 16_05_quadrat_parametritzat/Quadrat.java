d8d486d4777080d21eff3ed91a1c68fea1a8645d
Iker <xshine555@gmail.com>
2024-12-09 11:45:57 +0100

    Comentarios

--- 16_05_quadrat_parametritzat/Quadrat.java
+++ 16_05_quadrat_parametritzat/Quadrat.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 16_03. Quadrats modulars
+ * Iker Rivera Garcia
+ * 09/12/2024
+ * Dibuja un cuadrado con la longitud que haya introducido el usuario.
+ */
+
 public class Quadrat {
     public static void main(String[] args) {
         dibuixaQuadrat(Integer.parseInt(args[0]));
