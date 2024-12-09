d227df36bfc382f3ab3ec1c13fadfa15dd2f0a5d
Iker <xshine555@gmail.com>
2024-12-09 09:14:45 +0100

    Va

---
+++ 16_08_mostrainterval/MostraInterval.java
@@ -0,0 +1,31 @@
+public class MostraInterval {
+    public static void main(String[] args) {
+        System.out.println("text?");
+        String text = Entrada.readLine();
+        int textLen = text.length();
+
+        System.out.println("inici?");
+        int start = Integer.parseInt(Entrada.readLine());
+        start = Math.max(0, Math.min(textLen - 1, start));
+
+        System.out.println("final?");
+        int end = Integer.parseInt(Entrada.readLine());
+        end = Math.max(0, Math.min(textLen - 1, end));
+
+        mostraInterval(text, start, end);
+    }
+
+    public static void mostraInterval(String text, int inici, int fi) {
+        boolean isReverse = inici > fi;
+
+        if (isReverse) {
+            for (int i = inici; i >= fi; i--) {
+                System.out.println(text.charAt(i));
+            }
+        } else {
+            for (int i = inici; i <= fi; i++) {
+                System.out.println(text.charAt(i));
+            }
+        }
+    }
+}
