6a0d6a40c747cbad27e50b689a16b19d348e0f1d
Iker <xshine555@gmail.com>
2024-12-09 09:16:13 +0100

    Vale

---
+++ 16_09_cadena/CadenaContinua.java
@@ -0,0 +1,28 @@
+public class CadenaContinua {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+
+        if (text.isEmpty()) {
+            System.out.println("error");
+        } else {
+            System.out.println("Nombre?");
+            int num = Integer.parseInt(Entrada.readLine());
+
+            mostraCadenaContinua(text, num);
+        }
+    } 
+    
+    public static void mostraCadenaContinua(String text, int longitud) {
+        int textLen = text.length();
+
+        String result = "";
+
+        for (int i = 0; i < longitud; i++) {
+            char iChar = text.charAt(i % textLen);
+            result += iChar;
+        }
+
+        System.out.println(result);
+    }
+}
