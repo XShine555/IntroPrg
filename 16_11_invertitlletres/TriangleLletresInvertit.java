96a21792a8298ff5464cae6c6395e265b54c62e2
Iker <xshine555@gmail.com>
2024-12-09 11:52:54 +0100

    Comentarios

--- 16_11_invertitlletres/TriangleLletresInvertit.java
+++ 16_11_invertitlletres/TriangleLletresInvertit.java
@@ -1,3 +1,10 @@
+/*
+ * Exercici 16_11. Un triangle de lletres invertit
+ * Iker Rivera Garcia
+ * 09/12/2024
+ * Dibuja un triángulo invertido con las letras de la frase.
+ */
+
 public class TriangleLletresInvertit {
     public static void main(String[] args) {
         System.out.println("Text?");
