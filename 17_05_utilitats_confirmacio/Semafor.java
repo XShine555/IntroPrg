fe9a786b03ccc7b50ac06901fdcff35bbfcb7db4
XShine555 <87085480+XShine555@users.noreply.github.com>
2024-12-18 10:01:45 +0100

    Hola5

---
+++ 17_05_utilitats_confirmacio/Semafor.java
@@ -0,0 +1,29 @@
+/*
+ * Exercici 17_05. La guia de semàfors
+ * 18/12/2024
+ * Iker Rivera Garcia
+ * Programa que dependiendo del color del semáforo dice: espera, corre, pasa o ves al oculista.
+ */
+
+public class Semafor {
+	public static void main(String[] args) {
+		System.out.println("Color?");
+		String color = Entrada.readLine();
+		if (color.equals("vermell")) {
+			System.out.println("espera");
+		}
+		else if (color.equals("groc")) {
+			System.out.println("corre!");
+		}
+		else if (color.equals("verd")) {
+			System.out.println("passa");
+		}
+		else {
+			System.out.println("ves a l'oculista");
+		}
+	}
+
+    public static boolean respostaABoolean() {
+        return true;
+    }
+}
