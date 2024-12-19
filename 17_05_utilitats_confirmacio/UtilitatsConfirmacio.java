85b5580e3775c1bf5db7e3d38d82516e41947d73
prgtest <87085480+XShine555@users.noreply.github.com>
2024-12-19 09:34:00 +0100

    Hola5  ∾ prgtest autocommit 17_05 #7

--- 17_05_utilitats_confirmacio/UtilitatsConfirmacio.java
+++ 17_05_utilitats_confirmacio/UtilitatsConfirmacio.java
@@ -2,7 +2,7 @@ public class UtilitatsConfirmacio {
 	public static boolean respostaABoolean(String text) {
 		String[] upperCase = text.toUpperCase().split(" ");
         for (String string : upperCase) {
-            if (string.equals("SI"))
+            if (string.equals("SI") || string.equals("SÍ"))
                 return true;
         }
         return false;
