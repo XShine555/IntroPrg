5dd44dc6124fa1b04c5b953aa73edf5add199cc9
prgtest <87085480+XShine555@users.noreply.github.com>
2025-01-08 08:54:54 +0100

    hola  ∾ prgtest autocommit 17_11 #1

--- 17_11_es_enter_flexible/EsEnter.java
+++ 17_11_es_enter_flexible/EsEnter.java
@@ -1,4 +1,19 @@
 public class EsEnter {
+    public static void main(String[] args) {
+        System.out.println("Introdueix texts (enter sol per finalitzar)");
+        String text = Entrada.readLine();
+
+        while (!text.isBlank()) {
+            if (EsEnter.esEnter(text, false)) {
+                System.out.println("És enter");
+            } else {
+                System.out.println("No és enter");
+            }
+        }
+
+        System.out.println("Adéu");
+    }
+    
     public static boolean esEnter(String text, boolean estricte) {
         if (text.isBlank() || text.isEmpty())
             return false;
