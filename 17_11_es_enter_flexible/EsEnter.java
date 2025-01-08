49ed67b9df27875c2be9149bab2b4f6a42316efb
prgtest <87085480+XShine555@users.noreply.github.com>
2025-01-08 08:55:41 +0100

    hola  ∾ prgtest autocommit 17_11 #2

--- 17_11_es_enter_flexible/EsEnter.java
+++ 17_11_es_enter_flexible/EsEnter.java
@@ -4,7 +4,7 @@ public class EsEnter {
         String text = Entrada.readLine();
 
         while (!text.isBlank()) {
-            if (EsEnter.esEnter(text, false)) {
+            if (UtilString.esEnter(text, false)) {
                 System.out.println("És enter");
             } else {
                 System.out.println("No és enter");
@@ -12,64 +12,5 @@ public class EsEnter {
         }
 
         System.out.println("Adéu");
-    }
-    
-    public static boolean esEnter(String text, boolean estricte) {
-        if (text.isBlank() || text.isEmpty())
-            return false;
-
-        String sanitizeString = text;
-        if (!estricte) {
-            // Replace
-            for (int i = 0; i < text.length(); i++) {
-                char iChar = text.charAt(i);
-                if (!Character.isWhitespace(iChar)) {
-                    sanitizeString += iChar;
-                }
-            }
-        }
-
-        int textLen = sanitizeString.length();
-        for (int i = 0; i < textLen; i++) {
-            char iChar = sanitizeString.charAt(i);
-            boolean isSymbolOrInvalid = (iChar == '+' || iChar == '-' || iChar == '*' || iChar == '/'
-                || iChar == '%'
-                || iChar == '.' || iChar == '_') || Character.isLetter(iChar);
-            // Sí hay un símbolo al al final.
-            if (i == textLen - 1 && isSymbolOrInvalid) {
-                return false;
-            }
-            // Si es un símbolo comprobar que no tenga duplicados y sea válido.
-            else if (i > 0) {
-                char priorChar = sanitizeString.charAt(i - 1);
-                boolean isPriorSymbol = (priorChar == '+' || priorChar == '-' || priorChar == '*'
-                        || priorChar == '/' || priorChar == '%'
-                        || priorChar == '.' || priorChar == '_') || Character.isLetter(iChar);
-                if (isSymbolOrInvalid && isPriorSymbol) {
-                    return false;
-                }
-            }
-            // En caso de que empiece por "." O "-" es inválido.
-            else if (iChar == '.' || iChar == '_' || Character.isLetter(iChar)) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-
-    public static int aEnter(String text, boolean estricte) {
-        String sanitizeString = text;
-        if (!estricte) {
-            // Replace
-            for (int i = 0; i < text.length(); i++) {
-                char iChar = text.charAt(i);
-                if (!Character.isWhitespace(iChar)) {
-                    sanitizeString += iChar;
-                }
-            }
-        }
-
-        return Integer.parseInt(sanitizeString);
     }
 }
---
+++ 17_11_es_enter_flexible/UtilString.java
@@ -0,0 +1,60 @@
+public class UtilString {
+    public static boolean esEnter(String text, boolean estricte) {
+        if (text.isBlank() || text.isEmpty())
+            return false;
+
+        String sanitizeString = text;
+        if (!estricte) {
+            // Replace
+            for (int i = 0; i < text.length(); i++) {
+                char iChar = text.charAt(i);
+                if (!Character.isWhitespace(iChar)) {
+                    sanitizeString += iChar;
+                }
+            }
+        }
+
+        int textLen = sanitizeString.length();
+        for (int i = 0; i < textLen; i++) {
+            char iChar = sanitizeString.charAt(i);
+            boolean isSymbolOrInvalid = (iChar == '+' || iChar == '-' || iChar == '*' || iChar == '/'
+                || iChar == '%'
+                || iChar == '.' || iChar == '_') || Character.isLetter(iChar);
+            // Sí hay un símbolo al al final.
+            if (i == textLen - 1 && isSymbolOrInvalid) {
+                return false;
+            }
+            // Si es un símbolo comprobar que no tenga duplicados y sea válido.
+            else if (i > 0) {
+                char priorChar = sanitizeString.charAt(i - 1);
+                boolean isPriorSymbol = (priorChar == '+' || priorChar == '-' || priorChar == '*'
+                        || priorChar == '/' || priorChar == '%'
+                        || priorChar == '.' || priorChar == '_') || Character.isLetter(iChar);
+                if (isSymbolOrInvalid && isPriorSymbol) {
+                    return false;
+                }
+            }
+            // En caso de que empiece por "." O "-" es inválido.
+            else if (iChar == '.' || iChar == '_' || Character.isLetter(iChar)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    public static int aEnter(String text, boolean estricte) {
+        String sanitizeString = text;
+        if (!estricte) {
+            // Replace
+            for (int i = 0; i < text.length(); i++) {
+                char iChar = text.charAt(i);
+                if (!Character.isWhitespace(iChar)) {
+                    sanitizeString += iChar;
+                }
+            }
+        }
+
+        return Integer.parseInt(sanitizeString);
+    }
+}
