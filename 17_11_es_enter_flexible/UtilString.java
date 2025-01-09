6db673dda212de16cfa5cfdac5e510c868864cac
prgtest <87085480+XShine555@users.noreply.github.com>
2025-01-09 10:21:40 +0100

    Cambios  ∾ prgtest autocommit 17_11 #17

--- 17_11_es_enter_flexible/UtilString.java
+++ 17_11_es_enter_flexible/UtilString.java
@@ -52,28 +52,8 @@ public class UtilString {
             }
             return esEnter(sanitizeString);
         }
-        else {
-            for (int i = 0; i < text.length(); i++) {
-                char iChar = text.charAt(i);
-    
-                boolean isSymbol = iChar == '+' || iChar == '-' || iChar == '*' || iChar == '/' || iChar == '%';
-    
-                if (isSymbol && i == text.length() - 1) {
-                    return false;
-                } else if (!Character.isDigit(iChar) && !isSymbol) {
-                    return false;
-                } else if (i > 0) {
-                    char lastChar = text.charAt(i - 1);
-                    boolean isLastCharSymbol = lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/'
-                            || lastChar == '%';
-    
-                    if (isLastCharSymbol && isSymbol) {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
+        
+        return esEnter(text);
     }
 
     public static int aEnter(String text, boolean estricte) {
