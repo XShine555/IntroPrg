7d558c3e9f6f4d54a1fb93e36ead66dcf50428b8
prgtest <87085480+XShine555@users.noreply.github.com>
2025-01-09 10:25:18 +0100

    Cambios  ∾ prgtest autocommit 17_11 #22

--- 17_11_es_enter_flexible/UtilString.java
+++ 17_11_es_enter_flexible/UtilString.java
@@ -3,37 +3,43 @@ public class UtilString {
         if (text.isBlank() || text.isEmpty())
             return false;
 
-        for (int i = 0; i < text.length(); i++) {
+        int textLen = text.length();
+        for (int i = 0; i < textLen; i++) {
             char iChar = text.charAt(i);
-            if (Character.isWhitespace(iChar))
+            if (Character.isWhitespace(iChar)) {
                 return false;
+            }
 
-            boolean isSymbol = iChar == '+' || iChar == '-' || iChar == '*' || iChar == '/' || iChar == '%';
-
-            if (isSymbol && i == text.length() - 1) {
+            boolean isSymbolOrInvalid = (iChar == '+' || iChar == '-' || iChar == '*' || iChar == '/'
+                || iChar == '%'
+                || iChar == '.' || iChar == '_') || Character.isLetter(iChar);
+            // Sí hay un símbolo al al final.
+            if (i == textLen - 1 && isSymbolOrInvalid) {
                 return false;
-            } else if (!Character.isDigit(iChar) && !isSymbol) {
-                return false;
-            } else if (i > 0) {
-                char lastChar = text.charAt(i - 1);
-                boolean isLastCharSymbol = lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/'
-                        || lastChar == '%';
-
-                if (isLastCharSymbol && isSymbol) {
+            }
+            // Si es un símbolo comprobar que no tenga duplicados y sea válido.
+            else if (i > 0) {
+                char priorChar = text.charAt(i - 1);
+                boolean isPriorSymbol = (priorChar == '+' || priorChar == '-' || priorChar == '*'
+                        || priorChar == '/' || priorChar == '%'
+                        || priorChar == '.' || priorChar == '_') || Character.isLetter(iChar);
+                if (isSymbolOrInvalid && isPriorSymbol) {
                     return false;
                 }
             }
+            // En caso de que empiece por "." O "-" es inválido.
+            else if (iChar == '.' || iChar == '_' || Character.isLetter(iChar)) {
+                return false;
+            }
         }
+        
         return true;
     }
 
     public static boolean esEnter(String text, boolean estricte) {
-        if (text.isBlank() || text.isEmpty())
-            return false;
+        if (estricte)
+            return esEnter(text);
 
-        if (!estricte) 
-            text = text.replace(" ", "").replace(".", "").replace("_", "");
-        
         return esEnter(text);
     }
 
