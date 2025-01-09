989f2bc6dee8ec2934005d1b7ce6e8dda6eb3674
prgtest <87085480+XShine555@users.noreply.github.com>
2025-01-09 11:48:10 +0100

    Hi  ∾ prgtest autocommit 17_16 #2

--- 17_16_progressio/Progressio.java
+++ 17_16_progressio/Progressio.java
@@ -100,36 +100,6 @@ public class Progressio {
     }
 
     public static Character charToNormal(char character) {
-        if (Character.isLetter(character)) {
-            character = Character.toLowerCase(character);
-
-            switch (character) {
-                case 'à':
-                case 'á':
-                    return 'a';
-                case 'è':
-                case 'é':
-                    return 'e';
-                case 'ì':
-                case 'í':
-                case 'ï':
-                    return 'i';
-                case 'ò':
-                case 'ó':
-                case 'ö':
-                    return 'o';
-                case 'ù':
-                case 'ú':
-                case 'ü':
-                    return 'u';
-                case 'ñ':
-                    return 'n';
-                case 'ç':
-                    return 'c';
-                default:
-                    return character;
-            }
-        }
-        return character;
+        return UtilString.charToNormal(character);
     }
 }
