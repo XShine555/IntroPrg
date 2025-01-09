0d507f8babbf0d74013cabfe6148ade22fde790f
prgtest <87085480+XShine555@users.noreply.github.com>
2025-01-09 10:08:08 +0100

    Add  ∾ prgtest autocommit 17_10 #5

--- 17_10_es_enter/UtilString.java
+++ 17_10_es_enter/UtilString.java
@@ -11,6 +11,8 @@
 
         for (int i = 0; i < text.length(); i++) {
             char iChar = text.charAt(i);
+            if (Character.isWhitespace(iChar))
+                continue;
 
             boolean isSymbol = iChar == '+' || iChar == '-' || iChar == '*' || iChar == '/' || iChar == '%';
 
