a2b2743b1f195243ab6cbbaeee522eb6260dce01
prgtest <87085480+XShine555@users.noreply.github.com>
2025-01-09 10:10:39 +0100

    Cambios  ∾ prgtest autocommit 17_11 #3

--- 17_11_es_enter_flexible/UtilString.java
+++ 17_11_es_enter_flexible/UtilString.java
@@ -6,7 +6,7 @@ public class UtilString {
         int textLen = text.length();
         for (int i = 0; i < textLen; i++) {
             char iChar = text.charAt(i);
-            if (Character.isWhitespace(iChar) || iChar == '.') {
+            if (Character.isWhitespace(iChar) || iChar == '.' || iChar == '_') {
                 return false;
             }
 
