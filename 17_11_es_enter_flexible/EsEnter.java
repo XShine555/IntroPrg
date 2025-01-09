4485aea9b2fe09a12cbfebc6d36844ffd694052e
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-01-09 10:09:41 +0100

    Cambios

--- 17_11_es_enter_flexible/EsEnter.java
+++ 17_11_es_enter_flexible/EsEnter.java
@@ -1,9 +1,25 @@
 public class EsEnter {
     public static void main(String[] args) {
+        boolean isStrict = false;
+
+        if (args.length > 0) {
+            if (args[0].equals("estricte")) {
+                isStrict = true;
+            } else if (args[0].equals("flexible")) {
+                isStrict = false;
+            } else {
+                System.out.println("Modalitat no reconeguda");
+                return;
+            }
+        }
+
         System.out.println("Introdueix texts (enter sol per finalitzar)");
         String text = Entrada.readLine();
 
         while (!text.isBlank()) {
+            if (!isStrict)
+                text = text.replace(" ", "");
+
             if (UtilString.esEnter(text)) {
                 System.out.println("És enter");
             } else {
