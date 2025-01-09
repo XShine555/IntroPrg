97565516a7fa7060c9d73b489b8e63efa81738d6
prgtest <87085480+XShine555@users.noreply.github.com>
2025-01-09 10:34:50 +0100

    prgtest autocommit afegits fitxers de suport per 17_15

---
+++ 17_15_quants/Main.java
@@ -0,0 +1,14 @@
+ /* Aquest programa comprova el funcionament de la funció UtilSTring.quants() */
+ public class Main {
+     public static void main(String[] args) {
+         System.out.println("Text principal?");
+         String principal = Entrada.readLine();
+         System.out.println("Subtext?");
+         String subtext = Entrada.readLine();
+         System.out.println("Extricte?");
+         boolean extricte = UtilitatsConfirmacio.respostaABoolean(Entrada.readLine());
+         System.out.printf("quants(\"%s\", \"%s\", %b) -> %d%n",
+                           principal, subtext, extricte,
+                           UtilString.quants(principal, subtext, extricte));
+     }
+ }
