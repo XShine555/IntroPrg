b747b27ffced220e999a4dd872cff919b1be14b8
prgtest <87085480+XShine555@users.noreply.github.com>
2025-01-10 12:48:37 +0100

    Hi  ∾ prgtest autocommit 17_16 #53

--- 17_16_progressio/UtilString.java
+++ 17_16_progressio/UtilString.java
@@ -39,7 +39,7 @@ public class UtilString {
                 continue;
             }
 
-            if (lastIndex < (int) iChar) {
+            if (lastIndex <= (int) iChar) {
                 return false;
             }
 
