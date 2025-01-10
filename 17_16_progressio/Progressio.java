a371acde78013dfe0523d96e8ddccc931c4f1977
prgtest <87085480+XShine555@users.noreply.github.com>
2025-01-10 12:05:31 +0100

    Hi  ∾ prgtest autocommit 17_16 #29

--- 17_16_progressio/Progressio.java
+++ 17_16_progressio/Progressio.java
@@ -13,7 +13,7 @@ public class Progressio {
                 if (!Character.isWhitespace(iChar)) {
                     buffer += iChar;
                 } else {
-                    if (UtilString.textToNormal(buffer).length() <= 2) {
+                    if (UtilString.textToNormal(buffer).length() < 3) {
                         buffer = "";
                         continue;
                     }
