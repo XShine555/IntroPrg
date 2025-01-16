46c6d8448ad1679f98da60f1d6df45a84cec10d3
prgtest <87085480+XShine555@users.noreply.github.com>
2025-01-16 09:40:54 +0100

    T  ∾ prgtest autocommit 18_03 #5

--- 18_03_nota_mes_alta_v3/NotaMesAlta.java
+++ 18_03_nota_mes_alta_v3/NotaMesAlta.java
@@ -21,18 +21,28 @@ public class NotaMesAlta {
         }
 
         if (notes.length() > 1) {
+            String newNotes = "";
             String joinNotes = "";
             for (int i = 0; i < notes.length(); i++) {
-                char iChar = notes.charAt(i);
+                char iChar = newNotes.charAt(i);
+
+                if (iChar == (char)(higher + '0')) 
+                    continue;
+
+                newNotes += iChar;
+            }
+
+            for (int i = 0; i < newNotes.length(); i++) {
+                char iChar = newNotes.charAt(i);
 
                 if (iChar == (char)(higher + '0')) 
                     continue;
 
                 joinNotes += iChar;
 
-                if (i == notes.length() - 2)
+                if (i == newNotes.length() - 2)
                     joinNotes += " i ";
-                else if (i < notes.length() - 1)
+                else if (i < newNotes.length() - 1)
                     joinNotes += ", ";
             }
 
