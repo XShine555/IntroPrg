308bdbbc9e8bd652cbf8c6b6515b5c9c8d08bead
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-01-16 09:29:51 +0100

    Hecho

---
+++ 18_02_nota_mes_alta_v2/NotaMesAlta.java
@@ -0,0 +1,41 @@
+public class NotaMesAlta {
+    public static void main(String[] args) {
+        System.out.println("Introdueix les notes (-1 per finalitzar)");
+        String input = Entrada.readLine();
+        String notes = "";
+
+        int higher = 0;
+
+        while (!input.isBlank()) {
+            int number = Integer.parseInt(input);
+
+            if (number < 0 || number > 10)
+                break;
+
+            if (number > higher)
+                higher = number;
+
+            notes += number;
+
+            input = Entrada.readLine();
+        }
+
+        if (notes.length() > 1) {
+            System.out.println("La nota més alta és " + higher);
+
+            String joinNotes = "";
+            for (int i = 0; i < notes.length(); i++) {
+                joinNotes += notes.charAt(i);
+
+                if (i == notes.length() - 1)
+                    joinNotes += ".";
+                else if (i < notes.length() - 1)
+                    joinNotes += ", ";
+            }
+
+            System.out.format("La nota més alta és %s de les introduïdes: %s", higher, joinNotes);
+        }
+        else
+            System.out.println("Com a mínim calen dues notes");
+    }
+}
