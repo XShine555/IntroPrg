f7e7c37704f88c6e9f3b43eb9c6de6104cd5d029
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-01-16 09:23:06 +0100

    a

---
+++ 18_01_nota_mes_alta_v1/NotaMesAlta.java
@@ -0,0 +1,23 @@
+public class NotaMesAlta {
+    public static void main(String[] args) {
+        System.out.println("Introdueix les notes (-1 per finalitzar)");
+        String input = Entrada.readLine();
+
+        int higher = 0;
+
+        while (!input.isBlank()) {
+            int number = Integer.parseInt(input);
+
+            if (number < 0 || number > 10)
+                break;
+
+            if (number > higher)
+                higher = number;
+
+            input = Entrada.readLine();
+        }
+
+
+        System.out.println("La nota més alta és: " + higher);
+    }
+}
