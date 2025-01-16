b5a6f985dffdfd3c9cef8d4e05bd593eb77b07c0
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-01-16 11:41:43 +0100

    Hola

---
+++ 18_10_nomes_enters/SumaEnters.java
@@ -0,0 +1,42 @@
+public class SumaEnters {
+    public static void main(String[] args) {
+        int[] enters = filtraEnters(args);
+        int suma = sumaEnters(enters);
+        System.out.println(suma);
+    }
+
+    // nombre d'enters a valors
+    public static int quantsEnters(String[] valors) {
+        int cont = 0;
+        for (int i = 0; i < valors.length; i++) {
+            if (UtilString.esEnter(valors[i])) {
+                cont++;
+            }
+        }
+        return cont;
+    }
+
+    // enters que hi ha a valors
+    public static int[] filtraEnters(String[] valors) {
+        int index = 0;
+        int[] array = new int[quantsEnters(valors)];
+
+        for (int i = 0; i < valors.length; i++) {
+            if (UtilString.esEnter(valors[i])) {
+                array[index] = Integer.parseInt(valors[i]);
+                index++;
+            }
+        }
+
+        return array;
+    }
+
+    // suma dels valors
+    public static int sumaEnters(int[] valors) {
+        int suma = 0;
+        for (int i = 0; i < valors.length; i++) {
+            suma += valors[i];
+        }
+        return suma;
+    }
+}
