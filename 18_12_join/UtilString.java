4a24dbf598859d97dbf1ed0a6cb9beeb15aace22
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-01-17 12:14:35 +0100

    hola

---
+++ 18_12_join/UtilString.java
@@ -0,0 +1,29 @@
+public class UtilString {
+    // retorna un String format per les cadenes de text separades pel separador amb darrer separador
+    public static String junta(String[] cadenes, String separador, String darrerSeparador) {
+        if (cadenes.length < 1)
+            return "";
+
+        String result = "";
+
+        result += cadenes[0];
+
+        for (int i = 1; i < cadenes.length - 1; i++) {
+            if (i < cadenes.length - 1) {
+                result += separador;
+            }
+            result += cadenes[i];
+        }
+
+        if (cadenes.length > 2) {
+            result += darrerSeparador;
+            result += cadenes[cadenes.length - 1];
+        }
+
+        return result;
+    }
+    // equivalent a junta(cadenes, separador, separador)
+    public static String junta(String[] cadenes, String separador) {
+        return junta(cadenes, separador, separador);
+    }
+}
