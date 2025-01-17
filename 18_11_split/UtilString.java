2bc363e25602dfded3e3dbe58b8dc3f971276a52
prgtest <87085480+XShine555@users.noreply.github.com>
2025-01-17 11:57:09 +0100

    Am  ∾ prgtest autocommit 18_11 #1

--- 18_11_split/UtilString.java
+++ 18_11_split/UtilString.java
@@ -1,41 +1,44 @@
 public class UtilString {
+    private static int comptaParaules(String text, boolean inclouBlancs) {
+        if (text.isBlank() || text.isEmpty())
+            return 0;
+
+        int wordsCount = 1;
+
+        char previousChar = text.charAt(0);
+
+        for (int i = 1; i < text.length(); i++) {
+            char currentChar = text.charAt(i);
+
+            if (Character.isWhitespace(currentChar) && Character.isLetterOrDigit(previousChar)) {
+                wordsCount++;
+            } else if (Character.isLetterOrDigit(currentChar) && Character.isWhitespace(previousChar)) {
+                if (inclouBlancs) {
+                    wordsCount++;
+                }
+            }
+
+            previousChar = currentChar;
+        }
+
+        return wordsCount;
+    }
+
     // retorna la seqüència de subcadenes de text separades entre caràcters en
     // blanc, incloent els caracters en blanc quan inclouBlancs és cert
     public static String[] separa(String text, boolean inclouBlancs) {
         if (text.isBlank() || text.isEmpty())
             return new String[0];
 
-        int wordsCount = 0;
-        boolean wasLetterOrDigit = false;
-
-        for (int i = 0; i < text.length(); i++) {
-            char currentChar = text.charAt(i);
-
-            if (Character.isWhitespace(currentChar) && wasLetterOrDigit) {
-                if (inclouBlancs)
-                    wordsCount++;
-                wasLetterOrDigit = false;
-            }
-            else if (Character.isLetterOrDigit(currentChar) && !wasLetterOrDigit) {
-                wordsCount++;
-                wasLetterOrDigit = true;
-            }
-        }
-
+        int wordsCount = comptaParaules(text, inclouBlancs);
         int index = 0;
-        int sum = inclouBlancs ? 1 : 0;
 
-        String[] words = new String[wordsCount + sum];
+        String[] words = new String[wordsCount];
         String textBuffer = "";
 
         char previousChar = text.charAt(0);
 
-        for (int i = 0; i < text.length() + 1; i++) {
-            if (i > text.length() - 1) {
-                words[index] = textBuffer;
-                break;
-            }
-
+        for (int i = 0; i < text.length(); i++) {
             char currentChar = text.charAt(i);
 
             // Si este caracter es un espacio en blanco y el anterior no lo era entonces ha habido un cambio de palabra.
@@ -58,6 +61,12 @@ public class UtilString {
             textBuffer += currentChar;
 
             previousChar = currentChar;
+        }
+
+        System.out.println("Buffer: " + textBuffer + " Length: " + words.length + " Index: " + index + " WordsCount: " + wordsCount);
+
+        if (!textBuffer.isBlank()) {
+            words[index] = textBuffer;
         }
 
         return words;
