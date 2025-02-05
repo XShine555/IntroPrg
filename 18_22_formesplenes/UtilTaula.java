a200302cd8c5c37c92fe811fd620f9a6da6bcf50
prgtest <87085480+XShine555@users.noreply.github.com>
2025-02-05 12:52:54 +0100

    Gracias por no explicar  ∾ prgtest autocommit 18_22 #4

--- 18_22_formesplenes/UtilTaula.java
+++ 18_22_formesplenes/UtilTaula.java
@@ -310,8 +310,9 @@ public class UtilTaula {
             taula[i][i] = true;
             taula[i][taula[i].length - i - 1] = true;
 
-            if (i == taula[i].length - i - 1)
+            if (i == taula[i].length - i - 1 || taula[i].length - i - 1 == 0)
                 isDescending = true;
+            //System.out.println("i " + i + " op: " + (taula[i].length - i - 1) + " isDescending: " + isDescending);
 
             if (isDescending) {
                 for (int j = taula[i].length - i - 1; j < i; j++) {
