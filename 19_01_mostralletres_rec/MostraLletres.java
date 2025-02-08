26ad6c1141030779c1aa84a9f678b486f86bc471
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-02-08 16:17:30 +0100

    Maybe

---
+++ 19_01_mostralletres_rec/MostraLletres.java
@@ -0,0 +1,32 @@
+/*
+ * Iker Rivera Garcia
+ * 08/02/2025
+ * Clase MostraLletres, muestra las letras de un texto introducido por el usuario usando una función recursiva.
+ */
+public class MostraLletres {
+    public static void main(String[] args){
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        mostraLletres(text);
+        System.out.println();   // un salt de línia final
+    }
+
+    // mostra només les lletres de text
+    public static void mostraLletres(String text) {
+        if (text.isEmpty()) {      // cas base
+            return;
+        }
+
+        // hi ha al menys un caràcter. Comptem quantes lletres hi queden
+
+        // considerem el primer caràcter
+        char primer = text.charAt(0);
+        if (Character.isLetter(primer)) {
+            System.out.println(primer);
+        }
+
+        // considerem les lletres que conté la resta del text
+        String restaText = text.substring(1);  // resta del text
+        mostraLletres(restaText);      // crida recursiva
+    }
+}
