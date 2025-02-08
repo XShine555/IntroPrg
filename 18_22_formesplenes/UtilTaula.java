7e23e89161d5441007683d597cceb9c9b3159f2e
prgtest <87085480+XShine555@users.noreply.github.com>
2025-02-08 15:26:01 +0100

    Gracias por no explicar  ∾ prgtest autocommit 18_22 #9

--- 18_22_formesplenes/UtilTaula.java
+++ 18_22_formesplenes/UtilTaula.java
@@ -226,7 +226,7 @@ public class UtilTaula {
                 }
                 }
             else if (i < mid) {
-                for (int j = taula[i].length / 2; j < taula[i].length; j++) {
+                for (int j = taula[i].length / 2 - 1; j < taula[i].length; j++) {
                     taula[i][j] = true;
                 }
             }
