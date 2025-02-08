73f863a8c94bca8f5d49d6bb27dae2d502438ca9
prgtest <87085480+XShine555@users.noreply.github.com>
2025-02-08 15:35:43 +0100

    Gracias por no explicar  ∾ prgtest autocommit 18_22 #18

--- 18_22_formesplenes/UtilTaula.java
+++ 18_22_formesplenes/UtilTaula.java
@@ -97,18 +97,18 @@ public class UtilTaula {
     public static void inicialitzaQuartsNOPle(boolean[][] taula) {
         clearTable(taula);
 
-        int mid = taula.length / 2;
+        int mid = taula.length % 2 != 0 ? taula.length / 2 : taula.length / 2 - 1;
 
         for (int i = 0; i < taula.length; i++) {
-            if (i < mid) {
+            if (i > mid) {
+                int plus = taula[i].length % 2 != 0 ? 0 : 1;
+                taula[i][taula[i].length / 2 - plus] = true;
+            }
+            else if (i < mid) {
                 int plus = taula[i].length % 2 == 0 ? 0 : 1;
-
                 for (int j = 0; j < taula[i].length / 2 + plus; j++) {
                     taula[i][j] = true;
                 }
-            }
-            else if (i > mid) {
-                taula[i][taula[i].length / 2] = true;
             }
             else {
                 for (int j = 0; j < taula[i].length; j++) {
