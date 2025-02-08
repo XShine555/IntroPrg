daf652f4c9758d0b31433c55ea1bf0cbdd8b59a6
prgtest <87085480+XShine555@users.noreply.github.com>
2025-02-08 15:42:14 +0100

    Gracias por no explicar  ∾ prgtest autocommit 18_22 #24

--- 18_22_formesplenes/UtilTaula.java
+++ 18_22_formesplenes/UtilTaula.java
@@ -64,18 +64,7 @@ public class UtilTaula {
     }
 
     public static void inicialitzaVerticalMigSegonPle(boolean[][] taula) {
-        clearTable(taula);
-
-        if (taula.length == 4)
-        {
-            for (int i = 0; i < taula.length; i++) {
-                for (int j = taula[i].length / 2; j < taula[i].length; j++) {
-                    taula[i][j] = true;
-                }
-            }
-            taula[0][3] = true;
-            return;
-        }
+        //clearTable(taula);
 
         for (int i = 0; i < taula.length; i++) {
             for (int j = taula[i].length / 2; j < taula[i].length; j++) {
