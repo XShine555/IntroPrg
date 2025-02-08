36d3822964d4a3b773497c54941a515771eb019f
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-02-08 16:27:55 +0100

    Add

---
+++ 19_04_preordre/PreOrdre.java
@@ -0,0 +1,16 @@
+public class PreOrdre {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        preOrdre(text);
+    }    
+
+    public static void preOrdre(String s) {
+        System.out.println(s.length() + ": " + s);
+        if (s.length() <= 1)
+            return;
+        int mid = s.length() / 2;
+        preOrdre(s.substring(0, mid));
+        preOrdre(s.substring(mid));
+    }
+}
