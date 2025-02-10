f4e2d874c4e463e7513cdfd4f1e45de745caaf26
prgtest <87085480+XShine555@users.noreply.github.com>
2025-02-10 12:15:15 +0100

    prgtest autocommit afegits fitxers de suport per 19_07

---
+++ 19_07_simplehtml/Main.java
@@ -0,0 +1,7 @@
+/* Programa que mostra l'ús de SimpleHtml.mostraTextAHtml(String) a partir de
+ * l'entrada estàndard */
+public class Main {
+    public static void main(String[] args) {
+        SimpleHtml.mostraTextAHtml(Entrada.readLine());
+    }
+}
