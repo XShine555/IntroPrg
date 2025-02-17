f0b2cf6031273898ab995a7d2df346bf0c94f08e
prgtest <87085480+XShine555@users.noreply.github.com>
2025-02-17 12:43:53 +0100

    Uno  ∾ prgtest autocommit 20_08 #10

--- 20_08_alumnes/CercaAlumnes.java
+++ 20_08_alumnes/CercaAlumnes.java
@@ -103,7 +103,7 @@ public class CercaAlumnes {
             // comprova si el criteri de cerca es troba dins del nom o
             // el email. Si és així, mostra'l
             String lowerName = alumne.nom.toLowerCase();
-            if (lowerName.startsWith(args[0]) || alumne.email.contains(args[0])) {
+            if (lowerName.contains(args[0]) || alumne.email.contains(args[0])) {
                 mostraAlumne(alumne);
             }
         }
