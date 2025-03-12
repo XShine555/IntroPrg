c68b6fca2a8c8d58d19e989f281ab6f0a6ab47f1
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-03-12 10:04:53 +0100

    Su

---
+++ 30_09_extern/Ascensor.java
@@ -0,0 +1,3 @@
+public class Ascensor {
+    public int pis = -1;    
+}
---
+++ 30_09_extern/UsaAscensor.java
@@ -0,0 +1,6 @@
+public class UsaAscensor {
+    public static void main(String[] args) {
+        Ascensor ascensor = new Ascensor();
+        System.out.format("L'ascensor creat des de fora està al pis %s%n", ascensor.pis);
+    }
+}
