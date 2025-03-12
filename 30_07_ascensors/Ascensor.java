0643aed62ae75dbccf7299cd46699d4f4cd352f5
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-03-12 09:57:07 +0100

    Si

---
+++ 30_07_ascensors/Ascensor.java
@@ -0,0 +1,25 @@
+/*
+ * Iker Rivera Garcia
+ * 12/03/2025
+ * Ascensor.java
+ */
+public class Ascensor {
+    int pis = 0;
+
+    public static Ascensor[] creaAscensorsEnEscala(int quants) {
+        Ascensor[] col = new Ascensor[quants];
+        for (int i = 0; i < quants; i++) {
+            col[i] = new Ascensor();
+            col[i].pis = i;
+        }
+        return col;
+    }
+
+    public static void main(String[] args){
+        int quants = Integer.parseInt(Entrada.readLine());
+        Ascensor[] ascensors = creaAscensorsEnEscala(quants);
+        for (int i = 0; i < ascensors.length; i++) {
+            System.out.printf("Ascensor %d al pis %d%n", i, ascensors[i].pis);
+        }
+    }
+}
