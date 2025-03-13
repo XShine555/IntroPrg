2239e270af66bfcdfd04ecec3c90c3d639b8b449
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-13 09:11:16 +0100

    QUizas si  ∾ prgtest autocommit 30_10 #2

--- 30_10_posicio/GatRenat.java
+++ 30_10_posicio/GatRenat.java
@@ -1,3 +1,9 @@
+/*
+ * Iker Rivera Garcia
+ * 12/03/2025
+ * GatRenat.java, Classe que representa un gat amb 7 vides i una posició inicial d'estirat.
+ */
+
 public class GatRenat {
     int vides = 7;         // vides disponibles del gat Renat
     String posicio = "estirat";
