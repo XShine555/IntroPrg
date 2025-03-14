e6fe21e6a3eb5c998510bc2c778a8b631af681d5
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-14 12:24:29 +0100

    am  ∾ prgtest autocommit 30_14 #5

--- 30_14_ascensor_protegit/UsaAscensor.java
+++ 30_14_ascensor_protegit/UsaAscensor.java
@@ -11,13 +11,11 @@ public class UsaAscensor {
             System.out.println("Falten arguments");
             return;
         }
-        if (!UtilString.esEnter(args[0])) {
-            System.out.println("El primer argument ha de ser un enter");
-            return;
-        }
         System.out.println("Pis inicial: " + ascensor.getPis());
         System.out.println("Moviment inicial: " + ascensor.getMoviment());
-        ascensor.setPis(Integer.parseInt(args[0]));
+        if (UtilString.esEnter(args[0])) {
+            ascensor.setPis(Integer.parseInt(args[0]));
+        }
         ascensor.setMoviment(args[1]);
         System.out.println("Pis final: " + ascensor.getPis());
         System.out.println("Moviment final: " + ascensor.getMoviment());
