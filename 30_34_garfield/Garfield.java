59cf4dce1e4db65d599e23c075bb0e5429e6facc
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-14 12:50:42 +0100

    A  ∾ prgtest autocommit 30_34 #3

--- 30_34_garfield/Garfield.java
+++ 30_34_garfield/Garfield.java
@@ -2,4 +2,14 @@ public class Garfield extends Gat {
     public Garfield() {
         super("Garfield");
     }
+
+    @Override
+    public String estirat() {
+        if (this.getPosicio().equals("estirat")) {
+            return "Bai Maitea, bai";
+        }
+        
+        this.setPosicio("estirat");
+        return "ja m'estiro";
+    }
 }
