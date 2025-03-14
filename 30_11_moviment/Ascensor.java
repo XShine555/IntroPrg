63795149a44c82970eb2238c3f92b246b9110d99
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-03-14 11:51:33 +0100

    Epa

---
+++ 30_11_moviment/Ascensor.java
@@ -0,0 +1,10 @@
+/*
+ * Iker Rivera Garcia
+ * 12/03/2025
+ * Ascensor.java, Classe que representa un ascensor amb un pis inicial de -1 i un moviment inicial aturat.
+ */
+
+public class Ascensor {
+    int pis = -1;
+    String moviment = "aturat";
+}
---
+++ 30_11_moviment/UsaAscensor.java
@@ -0,0 +1,14 @@
+/*
+ * Iker Rivera Garcia
+ * 12/03/2025
+ * UsaAscensor.java, Usa la classe Ascensor per a mostrar el pis inicial de l'ascensor.
+ */
+public class UsaAscensor {
+    public static void main(String[] args) {
+        var ascensor = new Ascensor();
+        System.out.format("Pis inicial", ascensor.pis);
+        System.out.format("Moviment inicial: %s%n", ascensor.moviment);
+        ascensor.moviment = "pujant";
+        System.out.format("Moviment final: %s%n", ascensor.moviment);
+    }   
+}
