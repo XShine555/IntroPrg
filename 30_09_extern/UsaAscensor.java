a37fa38c6ad533605537db8f0b92cca51611e284
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-14 11:47:51 +0100

    Su  ∾ prgtest autocommit 30_09 #1

--- 30_09_extern/UsaAscensor.java
+++ 30_09_extern/UsaAscensor.java
@@ -1,3 +1,9 @@
+/*
+ * Iker Rivera Garcia
+ * 12/03/2025
+ * UsaAscensor.java, Usa la classe Ascensor per a mostrar el pis inicial de l'ascensor.
+ */
+
 public class UsaAscensor {
     public static void main(String[] args) {
         Ascensor ascensor = new Ascensor();
