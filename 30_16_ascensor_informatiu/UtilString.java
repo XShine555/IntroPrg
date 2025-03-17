3238717fd683d50f07e73fd61c5237f27f5396cf
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-17 11:49:39 +0100

    Si  ∾ prgtest autocommit 30_16 #1

---
+++ 30_16_ascensor_informatiu/UtilString.java
@@ -0,0 +1,31 @@
+/*
+ * Clase UtilString, contiene un metodo estatico para comprobar si un String en un numero entero valido.
+ */
+
+public class UtilString {
+    public static boolean esEnter(String text) {
+        if (text.isBlank() || text.isEmpty())
+            return false;
+
+        for (int i = 0; i < text.length(); i++) {
+            char iChar = text.charAt(i);
+
+            boolean isSymbol = iChar == '+' || iChar == '-' || iChar == '*' || iChar == '/' || iChar == '%';
+
+            if (isSymbol && i == text.length() - 1) {
+                return false;
+            } else if (!Character.isDigit(iChar) && !isSymbol) {
+                return false;
+            } else if (i > 0) {
+                char lastChar = text.charAt(i - 1);
+                boolean isLastCharSymbol = lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/'
+                        || lastChar == '%';
+
+                if (isLastCharSymbol && isSymbol) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    } 
+}
