7dc2f89a979b1d95012957ddfa2c798cc1717974
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-17 11:52:22 +0100

    Si  ∾ prgtest autocommit 30_16 #6

--- 30_16_ascensor_informatiu/Ascensor.java
+++ 30_16_ascensor_informatiu/Ascensor.java
@@ -26,12 +26,13 @@ public class Ascensor {
         return moviment;
     }
 
+    //esAbaix() i esAdalt() retornen cert quan l'ascensor es troba al primer i al darrer pis respectivament.
     public boolean esAbaix() {
-        return pis < 1;
+        return pis == 0;
     }
 
     public boolean esAdalt() {
-        return pis > 0;
+        return pis == 10;
     }
 
     public boolean esAturat() {
