aca3a64db66166b32c3c116ed021a15e0464f899
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-17 12:05:21 +0100

    qsi  ∾ prgtest autocommit 30_17 #7

--- 30_17_ensinistrat/GatRenat.java
+++ 30_17_ensinistrat/GatRenat.java
@@ -6,7 +6,7 @@
 
 public class GatRenat {
     private int vides = 7;
-    private String posicio = "estirat";
+    private String posicio = "";
     
     public void setVides(int value) {
         if (value >= 0 && value <= 10) {
