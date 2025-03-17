9c02970f8ae2acef4a827266b7487c0de062be01
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-17 12:14:49 +0100

    d1  ∾ prgtest autocommit 30_18 #7

--- 30_18_assessinable/GatRenat.java
+++ 30_18_assessinable/GatRenat.java
@@ -30,7 +30,7 @@ public class GatRenat {
     public String reviu(int i) {
         if (vides == 0) {
             vides = i;
-            return "guai!";
+            return "...";
         }
         else {
             return "...";
