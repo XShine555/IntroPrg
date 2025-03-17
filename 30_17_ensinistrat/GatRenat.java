456d7f3a43a826b72d501cc7d2589d4076aaad9e
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-17 11:58:04 +0100

    qsi  ∾ prgtest autocommit 30_17 #2

--- 30_17_ensinistrat/GatRenat.java
+++ 30_17_ensinistrat/GatRenat.java
@@ -8,6 +8,23 @@ public class GatRenat {
     private int vides = 7;
     private String posicio = "estirat";
 
+    public void setVides(int value) {
+        vides = value;
+    }
+
+    public int getVides() {
+        return vides;
+    }
+
+    public void setPosicio(String value) {
+        if (value.equals("dret") || value.equals("assegut") || value.equals("estirat"))
+            posicio = value;
+    }
+
+    public String getPosicio() {
+        return posicio;
+    }
+
     public String aixecat() {
         if (posicio.equals("aixecat"))
             return "passo de fer res";
