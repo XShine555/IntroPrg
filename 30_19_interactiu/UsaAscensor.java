a8adac96e3094682a4ebddf37e6e6442775c054a
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-18 09:34:33 +0100

    Si  ∾ prgtest autocommit 30_19 #29

--- 30_19_interactiu/UsaAscensor.java
+++ 30_19_interactiu/UsaAscensor.java
@@ -31,7 +31,7 @@
 
         // passem del pis indicat al la planta baixa
         System.out.println("Anant a la planta baixa quan està " + ascensor.comEsta());
-        while (! ascensor.esAdalt()) {
+        while (! ascensor.esAbaix()) {
             ascensor.seguentPis();       // passa al següent pis
             System.out.println("\t ara està pujant al pis "+ ascensor.getPis());
         }
