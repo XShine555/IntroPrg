d9fe44c31d9870afd633933d9cc47418c5e7fbde
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-18 09:35:32 +0100

    Si  ∾ prgtest autocommit 30_19 #30

--- 30_19_interactiu/UsaAscensor.java
+++ 30_19_interactiu/UsaAscensor.java
@@ -1,10 +1,4 @@
-/*
- * Iker Rivera Garcia
- * 15/03/2025
- * Classe UsaAscensor, classe que interactua amb l'usuari per a provar la classe Ascensor.
- */
-
- public class UsaAscensor {
+public class UsaAscensor {
     public static int llegeixEnter() {
         String resposta = Entrada.readLine();
         if (! UtilString.esEnter(resposta)) {
@@ -17,25 +11,24 @@
 
         System.out.println("Pis inicial");
         ascensor.setPis(llegeixEnter());
-        System.out.printf("Inicialment l'ascensor està aturat al pis %s%n", ascensor.getPis());
+        System.out.printf("Inicialment l'ascensor està %s%n", ascensor.getPis());
 
         // aturat a pujant
         boolean resposta = ascensor.arrencaAmunt();
-        System.out.printf("En arrencar amunt l'ascensor respon %b i està pujant al pis %s%n",
+        System.out.printf("En arrencar amunt l'ascensor respon %b i està %s%n",
                 resposta, ascensor.getPis());
 
         // demanem que baixi sense aturar
-        resposta = ascensor.arrencaAmunt();
-        System.out.printf("En arrencar a baix l'ascensor respon %b i està pujant al pis %s%n",
+        resposta = ascensor.arrencaAbaix();
+        System.out.printf("En arrencar a baix l'ascensor respon %b i està %s%n",
                 resposta, ascensor.getPis());
 
         // passem del pis indicat al la planta baixa
         System.out.println("Anant a la planta baixa quan està " + ascensor.comEsta());
         while (! ascensor.esAbaix()) {
             ascensor.seguentPis();       // passa al següent pis
-            System.out.println("\t ara està pujant al pis "+ ascensor.getPis());
+            System.out.println("\t ara està "+ ascensor.getPis());
         }
-
         ascensor.aturat();
         System.out.printf("Està %s. Final de trajecte!%n", ascensor.comEsta());
     }
