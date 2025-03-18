ed0da016af1ec8bb50115bf053a598366813cfb0
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-18 08:59:45 +0100

    Si  ∾ prgtest autocommit 30_19 #15

--- 30_19_interactiu/UsaAscensor.java
+++ 30_19_interactiu/UsaAscensor.java
@@ -4,10 +4,10 @@
  * Classe UsaAscensor, classe que interactua amb l'usuari per a provar la classe Ascensor.
  */
 
-public class UsaAscensor {
+ public class UsaAscensor {
     public static int llegeixEnter() {
         String resposta = Entrada.readLine();
-        if (!UtilString.esEnter(resposta)) {
+        if (! UtilString.esEnter(resposta)) {
             return -2;
         }
         return Integer.parseInt(resposta);
@@ -17,29 +17,23 @@ public class UsaAscensor {
 
         System.out.println("Pis inicial");
         ascensor.setPis(llegeixEnter());
-        System.out.printf("Inicialment l'ascensor està %s al pis %s%n", ascensor.getMoviment(), ascensor.getPis());
+        System.out.printf("Inicialment l'ascensor està %s%n", ascensor.getPis());
 
         // aturat a pujant
         boolean resposta = ascensor.arrencaAmunt();
-        System.out.printf("En arrencar amunt l'ascensor respon %b i està %s al pis %s%n",
-                resposta, ascensor.getMoviment(), ascensor.getPis());
+        System.out.printf("En arrencar amunt l'ascensor respon %b i està %s%n",
+                resposta, ascensor.getPis());
 
         // demanem que baixi sense aturar
-        resposta = ascensor.arrencaAmunt();
-        System.out.printf("En arrencar a baix l'ascensor respon %b i està %s al pis %s%n",
-                resposta, ascensor.getMoviment(), ascensor.getPis());
+        resposta = ascensor.arrencaAbaix();
+        System.out.printf("En arrencar a baix l'ascensor respon %b i està %s%n",
+                resposta, ascensor.getPis());
 
         // passem del pis indicat al la planta baixa
         System.out.println("Anant a la planta baixa quan està " + ascensor.comEsta());
-        while (! ascensor.esAdalt()) {
-            ascensor.seguentPis();       // passa al següent pis
-            System.out.println("\t ara està pujant al pis "+ ascensor.getPis());
-        }
-
-        ascensor.arrencaAbaix();
         while (! ascensor.esAbaix()) {
             ascensor.seguentPis();       // passa al següent pis
-            System.out.println("\t ara està baixant al pis "+ ascensor.getPis());
+            System.out.println("\t ara està "+ ascensor.getPis());
         }
         ascensor.aturat();
         System.out.printf("Està %s. Final de trajecte!%n", ascensor.comEsta());
