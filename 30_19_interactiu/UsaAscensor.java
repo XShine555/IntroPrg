3e81704fc619efba34f71ebbaeb49c2f83e8dd24
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-18 08:52:07 +0100

    Si  ∾ prgtest autocommit 30_19 #5

--- 30_19_interactiu/UsaAscensor.java
+++ 30_19_interactiu/UsaAscensor.java
@@ -17,7 +17,7 @@ public class UsaAscensor {
 
         System.out.println("Pis inicial");
         ascensor.setPis(llegeixEnter());
-        System.out.printf("Inicialment l'ascensor està %s as pis %s%n", ascensor.getMoviment(), ascensor.getPis());
+        System.out.printf("Inicialment l'ascensor està %s al pis %s%n", ascensor.getMoviment(), ascensor.getPis());
 
         // aturat a pujant
         boolean resposta = ascensor.arrencaAmunt();
