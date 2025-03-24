3b3c643926e00d23d135b0867d8dc360f984d64f
prgtest <87085480+XShine555@users.noreply.github.com>
2025-03-24 12:20:16 +0100

    Si  ∾ prgtest autocommit 30_26 #1

--- 30_26_hora/Hora.java
+++ 30_26_hora/Hora.java
@@ -2,9 +2,9 @@ public class Hora {
     private int hores;
     private int minuts;
     private int segons;
-    
+
     public Hora() {
-        this(0,0,0);
+        this(0, 0, 0);
     }
 
     public Hora(int hores, int minuts, int segons) {
@@ -116,5 +116,42 @@ public class Hora {
     @Override
     public String toString() {
         return String.format("%d:%02d:%02d", hores, minuts, segons);
+    }
+
+    /**
+     * Compara dues hores i retorna l'operador corresponent
+     * Per exemple, si hora1 és menor que hora2, l'operador serà "<". Els
+     * altres dos valors possibles són ">" i "=="
+     * 
+     * @param hora1: primera hora a comparar
+     * @param hora2: segona hora a comparar
+     * @return operador resultant
+     */
+    private static String composaOperadorComparacio(Hora hora1, Hora hora2) {
+        int comparacio = hora1.compareTo(hora2);
+        if (comparacio < 0) {
+            return "<";
+        } else if (comparacio > 0) {
+            return ">";
+        } else {
+            return "==";
+        }
+    }
+
+    public static void main(String[] args) {
+        Hora hora1 = new Hora();
+        Hora hora2 = new Hora(0, 0, 2);
+        System.out.printf("Inicialment hora1: %s %s hora2: %s%n",
+                hora1,
+                composaOperadorComparacio(hora1, hora2),
+                hora2);
+        System.out.println("Incrementem 1 segon a la primera i decrementem 1 segon a la segona");
+        hora1.incrementa();
+        hora2.decrementa();
+        System.out.printf("Finalment hora1: %s %s hora2: %s%n",
+                hora1,
+                composaOperadorComparacio(hora1, hora2),
+                hora2);
+
     }
 }
