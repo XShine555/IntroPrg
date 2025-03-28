a6429913856f20b3eb41e979ef4f02e01b143093
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-03-28 12:08:52 +0100

    3034

---
+++ 30_31_picarol/GatRenat.java
@@ -0,0 +1,88 @@
+/*
+ * Iker Rivera Garcia
+ * 26/03/2025
+ * Clase GatRenat, que tiene dos atributos, vides y posicio, y un constructor que inicializa los atributos.
+ */
+
+public class GatRenat {
+    private int vides;
+    private String posicio = "estirat";
+    private Picarol picarol;
+
+    public GatRenat() {
+        this(7, "estirat");
+    }
+
+    public GatRenat(int vides) {
+        this(vides, "estirat");
+    }
+
+    public GatRenat(String posicio) {
+        this(7, posicio);
+    }
+
+    public GatRenat(int vides, String posicio) {
+        setVides(vides);
+        setPosicio(posicio);
+    }
+
+    public Picarol agafaPicarol(Picarol newPicarol) {
+        Picarol lastPicarol = this.picarol;
+        this.picarol = newPicarol;
+        return lastPicarol;
+    }
+
+    public Picarol deixaPicarol() {
+        Picarol lastPicarol = this.picarol;
+        this.picarol = null;
+        return lastPicarol;
+    }
+
+    public boolean hiHaPicarol() {
+        return picarol != null;
+    }
+
+    @Override
+    public String toString() { return String.format("Vides: %d. Posició: %s", vides, posicio); }
+
+    public int getVides() { return vides; }
+    public String getPosicio() { return posicio; }
+
+    public void setVides(int vides) {
+        if (vides >= 0)  {
+            this.vides = vides;
+        }
+        else
+            this.vides = 7;
+    }
+
+    public void setPosicio(String posicio) {
+        if (posicio.equals("estirat") || posicio.equals("dret") || posicio.equals("assegut")) {
+            this.posicio = posicio;
+        }
+    }
+
+    public String aixecat() {
+        if (posicio.equals("dret"))
+            return "passo de fer res";
+
+        posicio = "dret";
+        return "ja m'aixeco";
+    }
+
+    public String seu() {
+        if (posicio.equals("assegut"))
+            return "passo de fer res";
+
+        posicio = "assegut";
+        return "ja m'assec";
+    }
+
+    public String estirat() {
+        if (posicio.equals("estirat"))
+            return "passo de fer res";
+
+        posicio = "estirat";
+        return "ja m'estiro";
+    }
+}
---
+++ 30_31_picarol/Picarol.java
@@ -0,0 +1,12 @@
+public class Picarol {
+    private int cops = 0;
+    
+    public void sona() {
+        System.out.println("clink-clink");
+        cops++;
+    }
+
+    public int vegades() {
+        return cops;
+    }
+}
