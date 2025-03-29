addafb25ae3c7fb754f686b16d1d0c023b4923cd
XShine555 <xshine555@users.noreply.github.com>
2025-03-29 22:07:36 +0100

    Si

--- 30_31_picarol/Picarol.java
+++ 30_31_picarol/Picarol.java
@@ -1,3 +1,11 @@
+/*
+ * Iker Rivera Garcia
+ * 29/03/2025
+ * Picarol.java,
+ * La clase tiene un método sona() que simula el sonido del picarol y
+ * incrementa el contador de veces que ha sonado.
+ */
+
 public class Picarol {
     private int cops = 0;
     
