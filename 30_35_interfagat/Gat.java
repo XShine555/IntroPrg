da30b780d95c1c2ed0318610de498ede197de619
prgtest <xshine555@users.noreply.github.com>
2025-04-05 20:50:15 +0200

    Si  ∾ prgtest autocommit 30_35 #17

--- 30_35_interfagat/Gat.java
+++ 30_35_interfagat/Gat.java
@@ -49,9 +49,9 @@ public class Gat implements EsserViu {
     public String reviu() {
         if (this.vides < VIDES_INICIALS) {
             this.vides++;
-            return "encara miolo";
-        } else {
             return "guai!";
+        } else {
+            return "encara miolo";
         }
     }
 }
