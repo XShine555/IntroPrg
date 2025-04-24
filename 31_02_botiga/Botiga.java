195b3c4827b9f33eed75ff1d0f18ceda8ad5401c
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-04-24 09:43:20 +0200

    Si

---
+++ 31_02_botiga/Botiga.java
@@ -0,0 +1,47 @@
+public class Botiga {
+    private static final int DEFAULT_MAX_VINS = 10;
+    private Vi[] vins;
+
+    public Botiga() {
+        this(DEFAULT_MAX_VINS);
+    }
+
+    public Botiga(int maxVins) {
+        this.vins = new Vi[maxVins];
+    }
+
+    public Vi afegeix(Vi vi) {
+        for (int i = 0; i < vins.length; i++) {
+            if (vins[i] == null) {
+                vins[i] = vi;
+                return vi;
+            }
+        }
+
+        return null;
+    }
+
+    public Vi elimina(Vi vi) {
+        for (int i = 0; i < vins.length; i++) {
+            if (vins[i] == vi) {
+                Vi eliminat = vins[i];
+                vins[i] = null;
+                return eliminat;
+            }
+        }
+
+        return null;
+    }
+
+    public Vi cerca(String nom) {
+        for (int i = 0; i < vins.length; i++) {
+            Vi vi = vins[i];
+
+            if (vi != null && vi.getNom().equals(nom)) {
+                return vi;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ 31_02_botiga/Vi.java
@@ -0,0 +1,100 @@
+/*
+ * Iker Rivera Garcia
+ * Exercici 31_01. La classe
+ * Vi.java
+ * Classe Vi, Representa un vi amb nom, preu i estoc
+ */
+
+public class Vi {
+    private static final int defaultEstoc = 0;
+    private static final int invalidValue = -1;
+    private static final String notValidNom = "NOM NO VÀLID!";
+
+    private String nom;
+    private int preu;
+    private int estoc;
+
+    public Vi(String nom, int preu) {
+        this(nom, preu, defaultEstoc);
+    }
+
+    public Vi(String nom, int preu, int estoc) {
+        this.nom = normalitzaNom(nom);
+
+        if (estoc < 0)
+            estoc = invalidValue;
+
+        if (preu < 0)
+            preu = invalidValue;
+
+        this.preu = preu;
+        this.estoc = estoc;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getPreu() {
+        return preu;
+    }
+
+    public void setPreu(int preu) {
+        if (preu < 0)
+            return;
+
+        this.preu = preu;
+    }
+
+    public int getEstoc() {
+        return estoc;
+    }
+
+    public void setEstoc(int estoc) {
+        if (estoc < 0)
+            return;
+
+        this.estoc = estoc;
+    }
+
+    public boolean esValid() {
+        return !nom.equals(notValidNom) && estoc != invalidValue && preu != invalidValue;
+    }
+
+    public static String normalitzaNom(String nom) {
+        if (nom == null || nom.isBlank()) {
+            return notValidNom;
+        }
+
+        String result = "";
+
+        for (int i = 0; i < nom.length(); i++) {
+            char thisChar = nom.charAt(i);
+
+            if (i > 0) {
+                char lastChar = nom.charAt(i - 1);
+
+                if (Character.isLetterOrDigit(thisChar)
+                        || (Character.isWhitespace(thisChar) && !Character.isWhitespace(lastChar))) {
+                    result += thisChar;
+                }
+            }
+            else {
+                if (Character.isLetterOrDigit(thisChar)) {
+                    result += thisChar;
+                }
+            }
+        }
+
+        return result.trim();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                    Vi: %s
+                    Preu: %d
+                    Estoc: %d
+                """, nom, preu, estoc);
+    }
+}
