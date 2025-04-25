98f9afb0990479cc5f91aef676e3d02c72f376cb
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-04-25 12:16:01 +0200

    Si

---
+++ 31_03_entorn/Entorn.java
@@ -0,0 +1,173 @@
+public class Entorn {
+    static class Result {
+        private int value;
+        private boolean wasBlank;
+        private boolean success;
+
+        public Result(int value, boolean wasBlank, boolean success) {
+            this.value = value;
+            this.wasBlank = wasBlank;
+            this.success = success;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public boolean wasBlank() {
+            return wasBlank;
+        }
+
+        public boolean isSuccess() {
+            return success;
+        }
+    }
+
+    private static Botiga botiga = new Botiga();
+
+    public static void main(String[] args) {
+        System.out.println("Celler La Bona Estrella. Escriviu ajuda per veure opcions.");
+        String comanda = "";
+
+        while (!comanda.equals("surt")) {
+            System.out.print("botiga> ");
+            comanda = Entrada.readLine();
+
+            switch (comanda) {
+                case "ajuda" -> ajuda();
+                case "cerca" -> cerca();
+                case "afegeix" -> afegeix();
+                case "modifica" -> modifica();
+                case "elimina" -> elimina();
+                case "surt" -> System.out.println("adéu");
+                default -> System.out.println("ERROR: comanda no reconeguda. Escriviu help per ajuda");
+            }
+        }
+    }
+
+    private static void elimina() {
+        System.out.print("nom (enter cancel·la)> ");
+        String nom = Entrada.readLine();
+
+        Vi result = botiga.cerca(nom);
+
+        if (result == null) {
+            System.out.print("ERROR: no s'ha pogut eliminar");
+            return;
+        }
+
+        System.out.print("A eliminar:");
+        System.out.print(result.toString());
+
+        System.out.print("Segur?> ");
+        String resposta = Entrada.readLine();
+
+        if (resposta.equalsIgnoreCase("Si") || resposta.equalsIgnoreCase("Sí")) {
+            Vi eliminat = botiga.elimina(nom);
+            System.out.println(eliminat != null ? "Eliminat" : "ERROR: no s'ha pogut eliminar");
+        } else {
+            System.out.println("No s'ha eliminat");
+        }
+    }
+
+    private static void afegeix() {
+        System.out.print("nom (enter cancel·la)> ");
+        String nom = Entrada.readLine();
+
+        if (nom.isBlank())
+            return;
+
+        System.out.print("preu (en cèntims)> ");
+        Result preu = converteixValor(Entrada.readLine());
+        if (preu.getValue() < 0) {
+            System.out.print("ERROR: cal un enter positiu");
+            return;
+        }
+
+        System.out.print("estoc (enter sense estoc)> ");
+        Result estoc = converteixValor(Entrada.readLine());
+        if (estoc.getValue() < 0) {
+            System.out.print("ERROR: cal un enter positiu");
+            return;
+        }
+
+        Vi vi = new Vi(nom, preu.getValue(), estoc.getValue());
+        Vi result = botiga.afegeix(vi);
+
+        System.out.print(
+                result != null ? String.format("Introduït:%s", result.toString()) : "ERROR: no s'ha pogut afegir");
+    }
+
+    private static void modifica() {
+        System.out.print("nom (enter cancel·la)> ");
+        String nom = Entrada.readLine();
+        if (nom.isBlank())
+            return;
+
+        System.out.print("preu (en cèntims)> ");
+        Result preu = converteixValor(Entrada.readLine());
+        if (!preu.isSuccess()) {
+            System.out.print("ERROR: cal un enter positiu");
+            return;
+        }
+
+        System.out.print("estoc (enter sense estoc)> ");
+        Result estoc = converteixValor(Entrada.readLine());
+        if (!estoc.isSuccess()) {
+            System.out.print("ERROR: cal un enter positiu");
+            return;
+        }
+
+        Vi result = botiga.cerca(nom);
+
+        if (result == null) {
+            System.out.print("ERROR: no s'ha trobat el vi");
+            return;
+        }
+
+        if (!preu.wasBlank())
+            result.setPreu(preu.getValue());
+
+        if (!estoc.wasBlank())
+            result.setEstoc(estoc.getValue());
+
+        System.out.print("Modificat:");
+        System.out.print(result.toString());
+    }
+
+    private static Result converteixValor(String valor) {
+        if (valor.isBlank())
+            return new Result(0, true, true);
+
+        try {
+            int value = Integer.parseInt(valor);
+            return new Result(value, false, true);
+        } catch (NumberFormatException e) {
+            return new Result(0, false, false);
+        }
+    }
+
+    private static void cerca() {
+        System.out.print("nom (enter cancel·la)> ");
+        String nom = Entrada.readLine();
+
+        if (nom.isBlank()) {
+            return;
+        }
+
+        Vi result = botiga.cerca(nom);
+        System.out.print(result != null ? String.format("Trobat:%s", result.toString()) : "No trobat");
+    }
+
+    private static void ajuda() {
+        System.out.print("""
+                Comandes disponibles:
+                ajuda
+                cerca
+                afegeix
+                modifica
+                elimina
+                surt
+                """);
+    }
+}
---
+++ 31_03_entorn/Vi.java
@@ -0,0 +1,113 @@
+/*
+ * Iker Rivera Garcia
+ * Exercici 31_01. La classe
+ * Vi.java
+ * Classe Vi, Representa un vi amb nom, preu i estoc
+ */
+
+public class Vi {
+    private static final int DEFAULT_ESTOC = 0;
+    private static final int INVALID_VALUE = -1;
+    private static final String INVALID_NAME = "NOM NO VÀLID!";
+    private static final char[] SPECIAL_CHARS = { '\'' };
+
+    private String nom;
+    private int preu;
+    private int estoc;
+
+    public Vi(String nom, int preu) {
+        this(nom, preu, DEFAULT_ESTOC);
+    }
+
+    public Vi(String nom, int preu, int estoc) {
+        this.nom = normalitzaNom(nom);
+
+        if (estoc < 0)
+            estoc = INVALID_VALUE;
+
+        if (preu < 0)
+            preu = INVALID_VALUE;
+
+        this.preu = preu;
+        this.estoc = estoc;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getPreu() {
+        return preu;
+    }
+
+    public void setPreu(int preu) {
+        if (preu < 0)
+            return;
+
+        this.preu = preu;
+    }
+
+    public int getEstoc() {
+        return estoc;
+    }
+
+    public void setEstoc(int estoc) {
+        if (estoc < 0)
+            return;
+
+        this.estoc = estoc;
+    }
+
+    public boolean esValid() {
+        return !nom.equals(INVALID_NAME) && estoc != INVALID_VALUE && preu != INVALID_VALUE;
+    }
+
+    public static String normalitzaNom(String nom) {
+        if (nom == null || nom.isBlank()) {
+            return INVALID_NAME;
+        }
+
+        String result = "";
+
+        for (int i = 0; i < nom.length(); i++) {
+            char thisChar = nom.charAt(i);
+
+            if (i > 0) {
+                char lastChar = nom.charAt(i - 1);
+
+                if (Character.isLetterOrDigit(thisChar)
+                        || (Character.isWhitespace(thisChar) && !Character.isWhitespace(lastChar))
+                        || isSpecialChar(thisChar)) {
+                    result += thisChar;
+                }
+            }
+            else {
+                if (Character.isLetterOrDigit(thisChar)) {
+                    result += thisChar;
+                }
+            }
+        }
+
+        return result.trim();
+    }
+
+    private static boolean isSpecialChar(char character) {
+        for (char specialChar : SPECIAL_CHARS) {
+            if (character == specialChar) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+            
+                    Vi: %s
+                    Preu: %d
+                    Estoc: %d
+                """, nom, preu, estoc);
+    }
+}
