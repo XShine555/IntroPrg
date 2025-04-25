0091fb78b9d30be1c0ff44752641a85fa9323cc3
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-04-25 12:42:34 +0200

    Si

---
+++ 31_04_csv/Botiga.java
@@ -0,0 +1,86 @@
+/*
+ * Iker Rivera Garcia
+ * Exercici 31_02. La classe
+ * Botiga.java
+ * Classe Botiga, representa una botiga de vins
+ */
+
+public class Botiga {
+    private static final int DEFAULT_MAX_VINS = 10;
+    private Vi[] vins;
+
+    public Botiga() {
+        this(DEFAULT_MAX_VINS);
+    }
+
+    public Botiga(int maxVins) {
+        this.vins = new Vi[maxVins];
+    }
+
+    private int currentIndex = 0;
+
+    public void iniciaRecorregut() {
+        currentIndex = 0;
+    }
+    
+    public Vi getSeguent() {
+        if (currentIndex >= vins.length) {
+            return null;
+        }
+
+        Vi vi = vins[currentIndex];
+        currentIndex++;
+
+        return vi;
+    }
+
+    public Vi afegeix(Vi vi) {
+        if (!vi.esValid())
+            return null;
+
+        if (cerca(vi.getNom()) != null)
+            return null;
+
+        for (int i = 0; i < vins.length; i++) {
+            if (vins[i] == null) {
+                vins[i] = vi;
+                return vi;
+            }
+        }
+
+        return null;
+    }
+
+    public Vi elimina(String nom) {
+        nom = Vi.normalitzaNom(nom);
+
+        for (int i = 0; i < vins.length; i++) {
+            Vi vi = vins[i];
+
+            if (vi != null && vi.getNom().equals(nom)) {
+                if (vi.getEstoc() > 0) {
+                    return null;
+                }
+
+                vins[i] = null;
+                return vi;
+            }
+        }
+
+        return null;
+    }
+
+    public Vi cerca(String nom) {
+        nom = Vi.normalitzaNom(nom);
+
+        for (int i = 0; i < vins.length; i++) {
+            Vi vi = vins[i];
+
+            if (vi != null && vi.getNom().equalsIgnoreCase(nom)) {
+                return vi;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ 31_04_csv/Entorn.java
@@ -0,0 +1,207 @@
+/*
+ * Iker Rivera Garcia
+ * Entorn.java
+ * Clase Entorn que simula un entorn de botiga
+ * per a gestionar vins.
+ * Aquesta classe permet afegir, modificar, eliminar i cercar vins.
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.annotation.processing.FilerException;
+
+public class Entorn {
+    private static final String CSV_FILE = "botiga.csv";
+    private static Botiga botiga = new Botiga();
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Celler La Bona Estrella. Escriviu ajuda per veure opcions.");
+        String comanda = "";
+
+        carregaCsv();
+
+        while (!comanda.equals("surt")) {
+            System.out.print("botiga> ");
+            comanda = Entrada.readLine();
+
+            switch (comanda) {
+                case "ajuda" -> ajuda();
+                case "cerca" -> cerca();
+                case "afegeix" -> afegeix();
+                case "modifica" -> modifica();
+                case "elimina" -> elimina();
+                case "surt" -> System.out.println("adéu");
+                default -> System.out.println("ERROR: comanda no reconeguda. Escriviu help per ajuda");
+            }
+        }
+    }
+
+    private static void carregaCsv() throws IOException {
+        File file = new File(CSV_FILE);
+        if (!file.exists()) {
+            System.out.println("Referències llegides: 0");
+            return;
+        }
+
+        FileReader fileReader = new FileReader(CSV_FILE);
+        BufferedReader input = new BufferedReader(fileReader);
+        int count = 0;
+
+        while (true) {
+            String linia = input.readLine();
+            if (linia == null)
+                break;
+
+            String[] parts = linia.split(";");
+            Vi result = botiga.afegeix(Vi.deArrayString(parts));
+
+            if (result != null)
+                count++;
+        }
+
+        System.out.println(String.format("Referències llegides: %s", count));
+    }
+
+    private static void elimina() {
+        System.out.print("nom (enter cancel·la)> ");
+        String nom = Entrada.readLine();
+        if (nom.isBlank())
+            return;
+
+        Vi result = botiga.cerca(nom);
+
+        if (result == null) {
+            System.out.println("No trobat");
+            return;
+        }
+
+        System.out.print("A eliminar:");
+        System.out.print(result.toString());
+
+        System.out.print("Segur?> ");
+        String resposta = Entrada.readLine();
+
+        if (resposta.equalsIgnoreCase("Si") || resposta.equalsIgnoreCase("Sí")) {
+            Vi eliminat = botiga.elimina(nom);
+            System.out.println(eliminat != null ? "Eliminat" : "ERROR: no s'ha pogut eliminar");
+        } else {
+            System.out.println("No eliminat");
+        }
+    }
+
+    private static void afegeix() {
+        System.out.print("nom (enter cancel·la)> ");
+        String nom = Entrada.readLine();
+
+        if (nom.isBlank())
+            return;
+
+        System.out.print("preu (en cèntims)> ");
+        String preuString = Entrada.readLine();
+        int preu = 0;
+
+        try{
+            preu = Integer.parseInt(preuString);
+
+            if (preu < 0) {
+                System.out.println("ERROR: cal un enter positiu");
+                return;
+            }
+        } catch (NumberFormatException e) { }
+
+        System.out.print("estoc (enter sense estoc)> ");
+        String estocString = Entrada.readLine();
+        int estoc = 0;
+        try{
+            estoc = Integer.parseInt(estocString);
+
+            if (estoc < 0) {
+                System.out.println("ERROR: cal un enter positiu");
+                return;
+            }
+        } catch (NumberFormatException e) { }
+
+        Vi vi = new Vi(nom, preu, estoc);
+        Vi result = botiga.afegeix(vi);
+
+        System.out.println(
+                result != null ? String.format("Introduït:%s", result.toString()) : "ERROR: no s'ha pogut afegir");
+    }
+
+    private static void modifica() {
+        System.out.print("nom (enter cancel·la)> ");
+        String nom = Entrada.readLine();
+        if (nom.isBlank())
+            return;
+
+        Vi result = botiga.cerca(nom);
+
+        if (result == null) {
+            System.out.println("No trobat");
+            return;
+        }
+
+        System.out.format("preu (enter %s)> ", result.getPreu());
+        String preuString = Entrada.readLine();
+        int preu = 0;
+
+        try{
+            preu = Integer.parseInt(preuString);
+
+            if (preu < 0) {
+                System.out.println("ERROR: cal un enter positiu");
+                return;
+            }
+        } catch (NumberFormatException e) { }
+
+        System.out.format("estoc (enter %s)> ", result.getEstoc());
+        String estocString = Entrada.readLine();
+        int estoc = 0;
+
+        try{
+            estoc = Integer.parseInt(estocString);
+
+            if (estoc < 0) {
+                System.out.println("ERROR: cal un enter positiu");
+                return;
+            }
+        } catch (NumberFormatException e) { }
+
+        if (!preuString.isBlank())
+            result.setPreu(preu);
+
+        if (!estocString.isBlank())
+            result.setEstoc(estoc);
+
+        System.out.print("Modificat:");
+        System.out.print(result.toString());
+    }
+
+    private static void cerca() {
+        System.out.print("nom (enter cancel·la)> ");
+        String nom = Entrada.readLine();
+
+        if (nom.isBlank()) {
+            return;
+        }
+
+        Vi result = botiga.cerca(nom);
+        System.out.println(result != null ? String.format("Trobat:%s", result.toString()) : "No trobat");
+    }
+
+    private static void ajuda() {
+        System.out.print("""
+                Comandes disponibles:
+                ajuda
+                cerca
+                afegeix
+                modifica
+                elimina
+                surt
+                """);
+    }
+}
