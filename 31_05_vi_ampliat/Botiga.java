4ed12163bf54123fe064b992a7c763172992e9ea
prgtest <xshine555@users.noreply.github.com>
2025-05-02 19:33:01 +0200

    Si  ∾ prgtest autocommit 31_05 #3

--- 31_05_vi_ampliat/Botiga.java
+++ 31_05_vi_ampliat/Botiga.java
@@ -84,4 +84,30 @@ public class Botiga {
 
         return null;
     }
+
+    public Vi cerca(Vi plantilla) {
+        for (Vi vi: vins) {
+            if (vi == null) continue;
+            if (! plantilla.getRef().isEmpty() && ! plantilla.getRef().equalsIgnoreCase(vi.getRef())) 
+                continue;
+            if (! plantilla.getNom().isEmpty() && ! plantilla.getNom().equalsIgnoreCase(vi.getNom())) 
+                continue;
+            if (plantilla.getPreu() >= 0 && plantilla.getPreu() <= vi.getPreu()) 
+                continue;
+
+            if (! plantilla.getLloc().isEmpty() && ! plantilla.getLloc().equalsIgnoreCase(vi.getLloc()))
+                continue;
+            if (! plantilla.getOrigen().isEmpty() && ! plantilla.getOrigen().equalsIgnoreCase(vi.getOrigen()))
+                continue;
+            if (! plantilla.getTipus().isEmpty() && ! plantilla.getTipus().equalsIgnoreCase(vi.getTipus()))
+                continue;
+            if (! plantilla.getCollita().isEmpty() && ! plantilla.getCollita().equalsIgnoreCase(vi.getCollita()))
+                continue;
+            if (plantilla.getEstoc() >= 0 && plantilla.getEstoc() <= vi.getEstoc()) 
+                continue;
+
+            return vi;
+        }
+        return null;
+    }
 }
