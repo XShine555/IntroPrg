82e327b7f20e1d111673f8bd55bd7cd9677b3c2f
XShine555 <xshine555@users.noreply.github.com>
2025-05-02 20:35:43 +0200

    Si

--- 31_09_botiga_excepcional/BotigaException.java
+++
@@ -1,9 +0,0 @@
-public class BotigaException extends Exception {
-    public BotigaException() {
-        super();
-    }
-
-    public BotigaException(String message) {
-        super(message);
-    }
-}
