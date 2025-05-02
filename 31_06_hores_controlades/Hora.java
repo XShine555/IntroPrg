07e4984b82b196d720576b7bf175a24f2cbd8041
XShine555 <xshine555@users.noreply.github.com>
2025-05-02 20:10:38 +0200

    Si

---
+++ 31_06_hores_controlades/Hora.java
@@ -0,0 +1,226 @@
+/*
+ * Iker Rivera Garcia
+ * 24/03/2025
+ * Clase Hora, que representa una hora del dia amb hores, minuts i segons i permet incrementar i decrementar la hora.
+ */
+
+public class Hora {
+    private int hores = 0;
+    private int minuts = 0;
+    private int segons = 0;
+
+    public Hora() {
+        this(0, 0, 0);
+    }
+
+    public Hora(int hores, int minuts, int segons) {
+        if (hores < 0 || minuts < 0 || segons < 0) {
+            return;
+        }
+        if (hores >= 24 || minuts >= 60 || segons >= 60) {
+            return;
+        }
+
+        setSegons(segons);
+        setMinuts(this.minuts + minuts);
+        setHores(this.hores + hores);
+    }
+
+    public int getHores() {
+        return hores;
+    }
+
+    public int getMinuts() {
+        return minuts;
+    }
+
+    public int getSegons() {
+        return segons;
+    }
+
+    public boolean setHores(int hora) {
+        if (hora < 0 || hora >= 24)
+        {
+            return false;
+        }
+        this.hores = hora;
+        return true;
+    }
+
+    public boolean setMinuts(int minuts) {
+        if (minuts < 0 || minuts >= 60)
+            return false;
+
+        this.minuts = minuts;
+        return true;
+    }
+
+    public boolean setSegons(int segons) {
+        if (segons < 0 || segons >= 60)
+            return false;
+        
+        this.segons = segons;
+        return true;
+    }
+
+    public void incrementa() {
+        segons++;
+
+        if (segons >= 60)
+        {
+            segons = 0;
+            minuts++;
+
+            if (minuts >= 60)
+            {
+                minuts = 0;
+                hores++;
+
+                if (hores >= 24)
+                {
+                    hores = 0;
+                }
+            }
+        }
+    }
+
+    public void incrementa(int value) {
+        if (value < 0)
+        {
+            decrementa(Math.abs(value));
+            return;
+        }
+
+        segons += value;
+
+        while (segons >= 60)
+        {
+            segons -= 60;
+            minuts++;
+
+            if (minuts >= 60)
+            {
+                minuts -= 60;
+                hores++;
+
+                if (hores >= 24)
+                {
+                    hores -= 24;
+                }
+            }
+        }
+    }
+
+    public void decrementa() {
+        segons--;
+
+        if (segons < 0)
+        {
+            segons = 59;
+            minuts--;
+
+            if (minuts < 0)
+            {
+                minuts = 59;
+                hores--;
+
+                if (hores < 0)
+                {
+                    hores = 23;
+                }
+            }
+        }
+    }
+
+    public void decrementa(int value) {
+        if (value < 0)
+        {
+            incrementa(Math.abs(value));
+            return;
+        }
+
+        segons -= value;
+
+        while (segons < 0)
+        {            
+            segons += 60;
+            minuts--;
+
+            if (minuts < 0)
+            {
+                minuts += 60;
+                hores--;
+
+                if (hores < 0)
+                {
+                    hores += 24;
+                }
+            }
+        }
+    }
+
+    public int compareTo(Hora hora) {
+        int comparacio = comparaValors(hores, hora.hores);
+        if (comparacio == 0) {
+            comparacio = comparaValors(minuts, hora.minuts);
+            if (comparacio == 0) {
+                comparacio = comparaValors(segons, hora.segons);
+            }
+        }
+        return comparacio;
+    }
+
+    private static int comparaValors(int p1, int p2)
+    {
+        if (p1 > p2)
+            return 1;
+        else if (p1 == p2)
+            return 0;
+        else
+            return -1;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d:%02d:%02d", hores, minuts, segons);
+    }
+
+    /**
+     * Compara dues hores i retorna l'operador corresponent
+     * Per exemple, si hora1 és menor que hora2, l'operador serà "<". Els
+     * altres dos valors possibles són ">" i "=="
+     * 
+     * @param hora1: primera hora a comparar
+     * @param hora2: segona hora a comparar
+     * @return operador resultant
+     */
+    private static String composaOperadorComparacio(Hora hora1, Hora hora2) {
+        int comparacio = hora1.compareTo(hora2);
+        if (comparacio < 0) {
+            return "<";
+        } else if (comparacio > 0) {
+            return ">";
+        } else {
+            return "==";
+        }
+    }
+
+    public static void main(String[] args) {
+        Hora hora1 = new Hora();
+        Hora hora2 = new Hora(0, 0, 2);
+        System.out.printf("Inicialment hora1: %s %s hora2: %s%n",
+                hora1,
+                composaOperadorComparacio(hora1, hora2),
+                hora2);
+        System.out.println("Incrementem 1 segon a la primera i decrementem 1 segon a la segona");
+        hora1.incrementa();
+        hora2.decrementa();
+        System.out.printf("Finalment hora1: %s %s hora2: %s%n",
+                hora1,
+                composaOperadorComparacio(hora1, hora2),
+                hora2);
+
+        //hora1.decrementa(1);
+        //System.out.println(hora1.toString());
+    }
+}
