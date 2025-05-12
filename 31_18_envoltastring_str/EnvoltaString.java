b9956b9bd01518531145c8117b6ff32278bf7008
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-05-12 11:38:07 +0200

    Si

---
+++ 31_18_envoltastring_str/EnvoltaString.java
@@ -0,0 +1,27 @@
+/*
+ * EnvoltaString.java
+ * Clase EnvoltaString, que encapsula una cadena de text i proporciona
+ * un mètode equals que compara dues instàncies de la classe sense tenir en compte
+ * les majúscules i minúscules.
+ */
+
+public class EnvoltaString {
+    private String string;
+
+    public EnvoltaString(String string) {
+        this.string = string;
+    }
+
+    public boolean equals(EnvoltaString other) {
+        return this.string.equalsIgnoreCase(other.string);
+    }
+
+    public boolean equals(String other) {
+        return this.string.equalsIgnoreCase(other);
+    }
+
+    @Override
+    public String toString() {
+        return "ENVOLTAT(" + "\"" + string + "\"" + ")";
+    }
+}
