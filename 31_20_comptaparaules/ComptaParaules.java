f525f5e06b7503b77d944a16264fa5c7fb3700b8
prgtest <87085480+XShine555@users.noreply.github.com>
2025-05-12 11:45:21 +0200

    Inicio  ∾ prgtest autocommit 31_20 #1

--- 31_20_comptaparaules/ComptaParaules.java
+++ 31_20_comptaparaules/ComptaParaules.java
@@ -2,7 +2,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ComptaParaules {
-    private Map<String, Integer> paraules = new HashMap<>();    
+    private static Map<String, Integer> paraules = new HashMap<>();    
 
-    
+    public static void main(String[] args) {
+        String[] text = Entrada.readLine().split(" ");
+
+        for (String string : text) {
+            String lowerCase = string.toLowerCase();
+            
+            if (paraules.containsKey(lowerCase)) {
+                paraules.put(lowerCase, paraules.get(lowerCase) + 1);
+            } else {
+                paraules.put(lowerCase, 1);
+            }
+        }
+    }
 }
