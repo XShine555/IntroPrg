0e06be8d2d87832846ca32925db878bc09b64b0d
prgtest <87085480+XShine555@users.noreply.github.com>
2025-05-12 11:46:35 +0200

    Inicio  ∾ prgtest autocommit 31_20 #2

--- 31_20_comptaparaules/ComptaParaules.java
+++ 31_20_comptaparaules/ComptaParaules.java
@@ -2,19 +2,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ComptaParaules {
-    private static Map<String, Integer> paraules = new HashMap<>();    
+    private static Map<String, Integer> paraules = new HashMap<>();
 
     public static void main(String[] args) {
-        String[] text = Entrada.readLine().split(" ");
+        String text = Entrada.readLine();
+        while (!text.isBlank()) {
+            String[] split = text.split(" ");
+            for (String string : split) {
+                String lowerCase = string.toLowerCase();
 
-        for (String string : text) {
-            String lowerCase = string.toLowerCase();
-            
-            if (paraules.containsKey(lowerCase)) {
-                paraules.put(lowerCase, paraules.get(lowerCase) + 1);
-            } else {
-                paraules.put(lowerCase, 1);
+                if (paraules.containsKey(lowerCase)) {
+                    paraules.put(lowerCase, paraules.get(lowerCase) + 1);
+                } else {
+                    paraules.put(lowerCase, 1);
+                }
             }
+
+            for (String string : paraules.keySet()) {
+                System.out.printf("%s -> %d%n", string, paraules.get(string));
+            }
+
+            text = Entrada.readLine();
         }
     }
 }
