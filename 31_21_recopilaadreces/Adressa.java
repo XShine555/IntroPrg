7fccc60dcb81e09890aab4c27934e2adef3bc60a
XShine555 <xshine555@users.noreply.github.com>
2025-05-13 21:49:40 +0200

    Uepa

--- 31_21_recopilaadreces/Adressa.java
+++ 31_21_recopilaadreces/Adressa.java
@@ -11,6 +11,10 @@ public class Adressa implements Comparable {
         return identificador;
     }
 
+    public String getDomini() {
+        return domini;
+    }
+
     public static boolean esValida(String text) {
         if (text == null)
             return false;
