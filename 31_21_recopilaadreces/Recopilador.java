bc6dddb96fc3a31f8520a874b43692fed633ac03
prgtest <xshine555@users.noreply.github.com>
2025-05-13 21:52:44 +0200

    Uepa  ∾ prgtest autocommit 31_21 #3

--- 31_21_recopilaadreces/Recopilador.java
+++ 31_21_recopilaadreces/Recopilador.java
@@ -2,27 +2,30 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.Map;
 
 public class Recopilador {
+    private Map<Adressa, List<String>> adreces = new TreeMap<>();
+
     private static final String SEPARADORS = "[\\s\\[{(<>})\\],;:'\"=|/\\!?]";
-    private Map<Adressa, Set<String>> adreces = new TreeMap<>();
 
     public int processa(String nom, String text) {
         if (text == null)
             return 0;
         int count = 0;
-        Set<Adressa> trobades = new HashSet<>();
+        List<Adressa> trobades = new ArrayList<>();
 
         for (String paraula : text.split(SEPARADORS)) {
             if (Adressa.esValida(paraula)) {
                 try {
                     Adressa a = Adressa.fromString(paraula);
                     if (!trobades.contains(a)) {
-                        adreces.computeIfAbsent(a, k -> new TreeSet<>()).add(nom);
+                        adreces.putIfAbsent(a, new ArrayList<>());
+                        if (!adreces.get(a).contains(nom)) {
+                            adreces.get(a).add(nom);
+                        }
                         trobades.add(a);
                         count++;
                     }
@@ -39,7 +42,7 @@ public class Recopilador {
     }
 
     public List<String> getNoms(Adressa a) {
-        List<String> llista = new ArrayList<>(adreces.getOrDefault(a, Set.of()));
+        List<String> llista = new ArrayList<>(adreces.getOrDefault(a, List.of()));
         Collections.sort(llista);
         return llista;
     }
