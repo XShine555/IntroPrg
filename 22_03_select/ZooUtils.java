551b61e3101ba0493ebd678f5066167540bc1ac6
prgtest <87085480+XShine555@users.noreply.github.com>
2025-05-19 10:49:41 +0200

    Primero  ∾ prgtest autocommit 22_03 #11

--- 22_03_select/ZooUtils.java
+++ 22_03_select/ZooUtils.java
@@ -10,7 +10,7 @@ public class ZooUtils {
 
         System.out.format("Nombre de categories: %s%n", array.length);
         for (int i = array.length - 1; i >= 0; i--) {
-            System.out.println("    " + array[i]);
+            System.out.println("  " + array[i]);
         }
     }
 }
