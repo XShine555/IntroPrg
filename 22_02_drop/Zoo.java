caa11c7e47f22075f3be4fcf00c9f3d8c712f2b6
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-05-19 10:31:35 +0200

    Si

---
+++ 22_02_drop/Zoo.java
@@ -0,0 +1,67 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Zoo {
+    private static final String NOM_BASE_DE_DADES = "animals.bd";
+    private static final String CADENA_DE_CONNEXIO = "jdbc:sqlite:" +
+            NOM_BASE_DE_DADES;
+    private Connection conn = null;
+
+    public void connecta() throws SQLException {
+        if (conn != null)
+            return; // ja connectat
+        conn = DriverManager.getConnection(CADENA_DE_CONNEXIO);
+    }
+
+    public void desconnecta() throws SQLException {
+        if (conn == null)
+            return; // ja desconnectat
+        conn.close();
+        conn = null;
+    }
+
+    public void creaTaulaCategories() throws SQLException {
+        String sql = "CREATE TABLE  CATEGORIES (" +
+                "       id        INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "       nom       VARCHAR(40))";
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate(sql);
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+
+    public void eliminaTaulaCategories() throws SQLException {
+        String sql = "DROP TABLE IF EXISTS CATEGORIES";
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate(sql);
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Zoo zoo = new Zoo();
+
+        System.out.print("Primer connectem amb la base de dades: ");
+        zoo.connecta();
+        System.out.println("connectat");
+
+        System.out.println("Creem la taula CATEGORIES");
+        zoo.creaTaulaCategories();
+
+        System.out.print("Finalment tanquem la connexió amb la base de dades: ");
+        zoo.desconnecta();
+        System.out.println("desconnectat");
+    }
+}
