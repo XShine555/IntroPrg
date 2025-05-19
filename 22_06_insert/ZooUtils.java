79ee756d59012c1d7ed9ee200b29813469bcd185
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-05-19 11:55:41 +0200

    Primeo

---
+++ 22_06_insert/ZooUtils.java
@@ -0,0 +1,22 @@
+/*
+ * Iker Rivera Garcia
+ * 19/05/2025
+ * Classe Categoria, que representa una categoria d'animal en un zoològic.
+ */
+
+import java.util.Collection;
+
+public class ZooUtils {
+    public static void mostraCategories(Collection<Categoria> categories) {
+        if (categories.isEmpty()) {
+            System.out.println("Cap categoria");
+            return;
+        }
+        Categoria[] array = categories.toArray(new Categoria[0]);
+
+        System.out.format("Nombre de categories: %s%n", array.length);
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.println("\t" + array[i]);
+        }
+    }
+}
