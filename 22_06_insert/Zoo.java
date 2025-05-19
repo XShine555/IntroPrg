49acd66807f15cd8205fe1466e393bccb3b092de
prgtest <87085480+XShine555@users.noreply.github.com>
2025-05-19 12:07:45 +0200

    Primero  ∾ prgtest autocommit 22_06 #7

--- 22_06_insert/Zoo.java
+++ 22_06_insert/Zoo.java
@@ -125,7 +125,7 @@ public class Zoo {
     }
 
     public List<Categoria> recuperaCategories() throws SQLException {
-        String sql = "SELECT * FROM CATEGORIES ORDER BY id ASC";
+        String sql = "SELECT * FROM CATEGORIES ORDER BY id DESC";
         Statement st = null;
         try {
             st = conn.createStatement();
