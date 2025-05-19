4f0cdd1f8709cb0c5b9e98a256ebe4651a979a9e
prgtest <87085480+XShine555@users.noreply.github.com>
2025-05-19 12:08:03 +0200

    Primero  ∾ prgtest autocommit 22_06 #9

--- 22_06_insert/Zoo.java
+++ 22_06_insert/Zoo.java
@@ -125,7 +125,7 @@ public class Zoo {
     }
 
     public List<Categoria> recuperaCategories() throws SQLException {
-        String sql = "SELECT * FROM CATEGORIES ORDER BY name DESC";
+        String sql = "SELECT * FROM CATEGORIES ORDER BY name DSC";
         Statement st = null;
         try {
             st = conn.createStatement();
