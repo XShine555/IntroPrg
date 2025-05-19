b1006795e8cf4c36cbc44fdf2daf0cc2e2e39efd
prgtest <87085480+XShine555@users.noreply.github.com>
2025-05-19 09:46:45 +0200

    First  ∾ prgtest autocommit 43_02 #1

--- 43_02_coordenada_y/TestPunt.java
+++ 43_02_coordenada_y/TestPunt.java
@@ -5,4 +5,18 @@ public class TestPunt {
         assert(punt.getX() == 0);
         assert(punt.getY() == 0);
     }
+
+    @org.junit.jupiter.api.Test
+    public void constructorDefecteXZero() {
+        Punt punt = new Punt();
+        assert(punt.getX() == 0);
+        assert(punt.getY() == 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void constructorDefecteXYPositiu() {
+        Punt punt = new Punt();
+        assert(punt.getX() >= 0);
+        assert(punt.getY() >= 0);
+    }
 }
