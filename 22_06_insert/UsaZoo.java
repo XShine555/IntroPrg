054ddbd45543140d7769ec5ed8111a6a8c3baec7
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-05-19 12:26:56 +0200

    Si

--- 22_06_insert/UsaZoo.java
+++ 22_06_insert/UsaZoo.java
@@ -3,9 +3,9 @@
  * 19/05/2025
  * Classe UsaZoo, que conté el mètode main per executar les operacions de la classe Zoo.
  */
-
 import java.sql.SQLException;
-
+import java.util.List;
+import java.util.Arrays;
 public class UsaZoo {
     public static void main(String[] args) throws SQLException {
         Zoo zoo = new Zoo();
@@ -27,20 +27,40 @@ public class UsaZoo {
         zoo.afegeixCategoria(new Categoria("ocell"));
         ZooUtils.mostraCategories(zoo.recuperaCategories());
 
+        // creem una llista d'animals amb un de repetit
+        List<Animal> animals = Arrays.asList(
+            new Animal("pardal", new Categoria("ocell")),
+            new Animal("gat", new Categoria("mamífer")),
+            new Animal("guppy", new Categoria("peix")),
+            new Animal("gat", new Categoria("mamífer"))
+            );
+
         System.out.println();
-        System.out.println("Afegim guppy amb " + peix);
-        zoo.afegeixAnimal(new Animal("guppy", peix));
+        System.out.println("Considerem els següents animals");
+        ZooUtils.mostraAnimals(animals);
+
+        // Afegim els animals
+        for (Animal animal: animals) {
+            zoo.afegeixAnimal(animal);
+        }
+
+        System.out.println();
+        System.out.println("Un cop afegits, els animals queden:");
+        ZooUtils.mostraAnimals(animals);
+
+        System.out.println();
+        System.out.println("A la base de dades, els animals són:");
+        ZooUtils.mostraAnimals(zoo.recuperaAnimals());
+
+        System.out.println();
+        System.out.println("A la base de dades, les categories són:");
         ZooUtils.mostraCategories(zoo.recuperaCategories());
 
         System.out.println();
-        System.out.println("Afegim pardal dins d'una categoria coneguda");
-        zoo.afegeixAnimal(new Animal("pardal", new Categoria("ocell")));
-        ZooUtils.mostraCategories(zoo.recuperaCategories());
-
-        System.out.println();
-        System.out.println("Afegim un animal d'una nova categoria");
-        zoo.afegeixAnimal(new Animal("gat", new Categoria("mamífer")));
-        ZooUtils.mostraCategories(zoo.recuperaCategories());
+        System.out.println("Recuperem ara alguns animals per nom");
+        System.out.println("El guppy: " + zoo.obteAnimalPerNom("guppy"));
+        System.out.println("El gat: " + zoo.obteAnimalPerNom("gat"));
+        System.out.println("El gat Renat: " + zoo.obteAnimalPerNom("Renat"));
 
         System.out.println();
         System.out.print("Finalment tanquem la connexió amb la base de dades: ");
