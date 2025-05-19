6a7d8c4d7a9734caaef05179ccc2aa819f96e372
prgtest <87085480+XShine555@users.noreply.github.com>
2025-05-19 09:50:26 +0200

    Si  ∾ prgtest autocommit 43_03 #1

--- 43_03_setter_y/UsaPunt.java
+++ 43_03_setter_y/UsaPunt.java
@@ -5,9 +5,21 @@
  */
 
 public class UsaPunt {
-     public static void main(String[] args){
+    public static void main(String[] args){
          Punt punt = new Punt();
+         int x = parseInt(args[0] == null ? null : args[0]); // args[0] si hi és i és enter, o 0 altrament
+         int y = parseInt(args[1] == null ? null : args[1]); //  args[1] si hi és i és enter, o 0 altrament
+         punt.setX(x);
+         punt.setY(y);
          System.out.printf("punt.getX() -> %d%n", punt.getX());
          System.out.printf("punt.getY() -> %d%n", punt.getY());
-     }
- }
+    }
+
+    private static int parseInt(String s) {
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}
