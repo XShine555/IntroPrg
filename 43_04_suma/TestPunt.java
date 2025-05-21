881f927cf68464e8818c09b41139e6402836519c
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-05-21 08:23:07 +0200

    Ya

--- 43_04_suma/TestPunt.java
+++ 43_04_suma/TestPunt.java
@@ -5,55 +5,58 @@
  * de les funcionalitats de la classe Punt.
  */
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class TestPunt {
-    @org.junit.jupiter.api.Test
+    @Test
     public void constructorDefecteYZero() {
         Punt punt = new Punt();
-        assert(punt.getX() == 0);
-        assert(punt.getY() == 0);
+        Assertions.assertEquals(0, punt.getX());
+        Assertions.assertEquals(0, punt.getY());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void constructorCustomParams() {
         Punt punt = new Punt(5, 10);
-        assert(punt.getX() == 5);
-        assert(punt.getY() == 10);
+        Assertions.assertEquals(5, punt.getX());
+        Assertions.assertEquals(10, punt.getY());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void sumaDosPunts() {
         Punt punt1 = new Punt(5, 10);
         Punt punt2 = new Punt(3, 7);
         punt1.suma(punt2);
-        assert(punt1.getX() == 8);
-        assert(punt1.getY() == 17);
+        Assertions.assertEquals(8, punt1.getX());
+        Assertions.assertEquals(17, punt1.getY());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void constructorDefecteXZero() {
         Punt punt = new Punt();
-        assert(punt.getX() == 0);
-        assert(punt.getY() == 0);
+        Assertions.assertEquals(0, punt.getX());
+        Assertions.assertEquals(0, punt.getY());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void xPositiu() {
         Punt punt = new Punt();
-        assert(punt.getX() >= 0);
+        Assertions.assertTrue(punt.getX() >= 0);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void yPositiu() {
         Punt punt = new Punt();
-        assert(punt.getY() >= 0);
+        Assertions.assertTrue(punt.getY() >= 0);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void comprobaSetters() {
         Punt punt = new Punt();
         punt.setX(5);
         punt.setY(10);
-        assert(punt.getX() == 5);
-        assert(punt.getY() == 10);
+        Assertions.assertEquals(5, punt.getX());
+        Assertions.assertEquals(10, punt.getY());
     }
 }
