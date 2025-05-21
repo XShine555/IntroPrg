dfc5f8245b2aeccc6a7dc802ab4cb76425ace632
XShine555 <87085480+XShine555@users.noreply.github.com>
2025-05-21 08:35:26 +0200

    Si

--- 43_05_segment/TestSegment.java
+++ 43_05_segment/TestSegment.java
@@ -5,35 +5,38 @@
  * de les funcionalitats de la classe Punt.
  */
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestSegment {
-    @org.junit.jupiter.api.Test
+    @Test
     public void constructorDefecte() {
         Segment segment = new Segment();
-        assert(segment.getP1().getX() == 0);
-        assert(segment.getP1().getY() == 0);
-        assert(segment.getP2().getX() == 0);
-        assert(segment.getP2().getY() == 0);
+        Assertions.assertEquals(0, segment.getP1().getX());
+        Assertions.assertEquals(0, segment.getP1().getY());
+        Assertions.assertEquals(0, segment.getP2().getX());
+        Assertions.assertEquals(0, segment.getP2().getY());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void constructorCustomParams() {
         Punt punt1 = new Punt(5, 10);
         Punt punt2 = new Punt(3, 7);
         Segment segment = new Segment(punt1, punt2);
-        assert(segment.getP1().getX() == 5);
-        assert(segment.getP1().getY() == 10);
-        assert(segment.getP2().getX() == 3);
-        assert(segment.getP2().getY() == 7);
+        Assertions.assertEquals(5, segment.getP1().getX());
+        Assertions.assertEquals(10, segment.getP1().getY());
+        Assertions.assertEquals(3, segment.getP2().getX());
+        Assertions.assertEquals(7, segment.getP2().getY());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void constructorCustomParamsXZero() {
         Punt punt1 = new Punt(5, 10);
         Punt punt2 = new Punt(0, 7);
         Segment segment = new Segment(punt1, punt2);
-        assert(segment.getP1().getX() == 5);
-        assert(segment.getP1().getY() == 10);
-        assert(segment.getP2().getX() == 0);
-        assert(segment.getP2().getY() == 7);
+        Assertions.assertEquals(5, segment.getP1().getX());
+        Assertions.assertEquals(10, segment.getP1().getY());
+        Assertions.assertEquals(0, segment.getP2().getX());
+        Assertions.assertEquals(7, segment.getP2().getY());
     }
 }
