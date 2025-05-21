0e31e1b098ec765031c9f32f8e9b64eb790f7e4b
prgtest <87085480+XShine555@users.noreply.github.com>
2025-05-21 09:27:07 +0200

    Dimision  ∾ prgtest autocommit 22_09 #3

--- 22_09_delete/Zoo.java
+++ 22_09_delete/Zoo.java
@@ -228,10 +228,8 @@ public class Zoo {
                 "       id        INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "       nom       VARCHAR(40)," +
                 "       categoria INTEGER," +
-                "       CONSTRAINT fk_categoria" + 
-                "       FOREIGN KEY (categoria)" + 
-                "       REFERENCES CATEGORIES(id))" +
-                "       ON DELETE CASCADE";
+                "       FOREIGN KEY (categoria) REFERENCES CATEGORIES(id)" +
+                "       ON DELETE CASCADE)";
         Statement st = null;
         try {
             st = conn.createStatement();
